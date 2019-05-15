aeb77fb475458bbac7ddf6310de885acb3544fb0
Deep Kamal Singh <deesingh@adobe.com>
2019-05-15 15:09:58 +0530

    CIF-755 - added JS method to switch page

---
+++ ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/productlist/v1/productlist/clientlibs/dist/js/navhelper.js
@@ -0,0 +1,30 @@
+let getCookie = function (cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+let setCookie = function (cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+
+let switchToPageNumber = function (pageNum) {
+
+    console.log('switchToPageNumber called with pageNum=' + pageNum);
+
+    window.location = "?page="+pageNum;
+
+};
