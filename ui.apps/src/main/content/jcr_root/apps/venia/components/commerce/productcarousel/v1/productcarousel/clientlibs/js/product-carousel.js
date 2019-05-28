da2aaf13f8763d351e53e8e4f5f199f97a14b492
Sandeep Aggarwal <sandagga@adobe.com>
2019-05-29 01:40:07 +0530

    Refactored to include root element using data attributes

--- ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/productcarousel/v1/productcarousel/clientlibs/js/product-carousel.js
+++ ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/productcarousel/v1/productcarousel/clientlibs/js/product-carousel.js
@@ -12,110 +12,120 @@
  *
  ******************************************************************************/
 
-(function () {
+(function() {
 
-	/**
-        *	Product Carousel Component	
-	*/    
-    class ProductCarousel {
+  /**
+   *  Product Carousel Component  
+   */
+  class ProductCarousel {
 
-		// Initialise the variables
-      constructor() {
-		this._speed             = 300;
-        this._delay             = 0;
-        this._effect            = 'linear';  
-        this._product_carousel_parent = document.querySelector('.product-carousel-parent');
-        this._cardsContainer = document.querySelector('.cards-container'); 
-        this._cards = this._cardsContainer.querySelectorAll('.product-card');
-        this._btnPrev = document.querySelector('#prev-btn');
-        this._btnNext = document.querySelector('#next-btn');
-        this._currentPos = 0;
+    // Initialise the variables
+    static selectors = {
 
-        this._minPos  = (this._product_carousel_parent.offsetWidth - (this._cards.length * this._cards[0].offsetWidth));
+      self: "[data-comp-is=productcarousel]",
+      btnPrev: "[data-carousel-action=prev]",
+      btnNext: "[data-carousel-action=next]"
 
-        this._maxPosIndex = ((this._cardsContainer.offsetWidth - this._product_carousel_parent.offsetWidth) / this._cards[0].offsetWidth)
-        this._cardsContainer.style.marginLeft = '0px';
-        this._cardsContainer.style.width = (this._cards[0].offsetWidth * this._cards.length) + 'px';
+    }
 
-        this._btnPrev.addEventListener("click", e => this._goToPrevProductCard());
-        this._btnNext.addEventListener("click", e => this._goToNextProductCard());
-        
-        if(this._minPos >= 0) {
-            this._btnNext.disabled = true;
-            this._btnPrev.disabled = true;
+    constructor(rootElement) {
+      this._speed = 300;
+      this._delay = 0;
+      this._effect = 'linear';
+      this._product_carousel_parent = rootElement.querySelector('.product-carousel-parent');
+      this._cardsContainer = rootElement.querySelector('.cards-container');
+      this._cards = this._cardsContainer.querySelectorAll('.product-card');
+      this._btnPrev = rootElement.querySelector(ProductCarousel.selectors.btnPrev);
+      this._btnNext = rootElement.querySelector(ProductCarousel.selectors.btnNext);
+      this._currentPos = 0;
 
-        }else {
-            this._btnPrev.disabled = true;
-        }
+      this._minPos = (this._product_carousel_parent.offsetWidth - (this._cards.length * this._cards[0].offsetWidth));
 
+      this._maxPosIndex = ((this._cardsContainer.offsetWidth - this._product_carousel_parent.offsetWidth) / this._cards[0].offsetWidth)
+      this._cardsContainer.style.marginLeft = '0px';
+      this._cardsContainer.style.width = (this._cards[0].offsetWidth * this._cards.length) + 'px';
+
+      this._btnPrev.addEventListener("click", e => this._goToPrevProductCard());
+      this._btnNext.addEventListener("click", e => this._goToNextProductCard());
+
+      if (this._minPos >= 0) {
+        this._btnNext.disabled = true;
+        this._btnPrev.disabled = true;
+
+      } else {
+        this._btnPrev.disabled = true;
+      }
+    }
+
+    // click event handler for Next Button
+    _goToNextProductCard() {
+
+      if (this._btnNext.disabled === false) {
+        var newCurrentPos = 0;
+        newCurrentPos = this._currentPos + 1;
+        this._goToProductCard(newCurrentPos, 'next');
+      }
+    };
+
+    // Click event handler for Prev Button  
+    _goToPrevProductCard() {
+
+      if (this._btnPrev.disabled === false) {
+        var newCurrentPos = 0;
+        newCurrentPos = this._currentPos - 1;
+        this._goToProductCard(newCurrentPos, 'prev');
+      }
+    };
+
+
+    // create product card and transition
+    _goToProductCard(n, dir) {
+
+      var cardwidth = this._cards[0].offsetWidth,
+        currentPos = Math.max(-cardwidth * this._currentPos, this._minPos),
+        scrollWidth = cardwidth,
+        newPos;
+
+      newPos = dir === 'next' ? Math.max(this._minPos, currentPos - scrollWidth) : Math.min(0, currentPos + scrollWidth);
+      this._cardsContainer.style.transition = 'margin-left ' + this._speed + 'ms' + ' ' + this._effect + ' ' + this._delay + 'ms';
+      this._cardsContainer.style.marginLeft = newPos + 'px';
+      this._currentPos = n;
+
+      this._btnNext.disabled = false;
+      this._btnPrev.disabled = false;
+
+
+      if (this._currentPos === this._maxPosIndex) {
+        this._btnNext.disabled = true;
+        this._btnPrev.disabled = false;
       }
 
-       // click event handler for Next Button
-	  _goToNextProductCard() {
-        var newCurrentPos = 0;
-        if (this._btnNext.disabled === false) {
-    
-            newCurrentPos = this._currentPos + 1;
-            this._goToProductCard(newCurrentPos, 'next');
-        }
-      };
-
-      // Click event handler for Prev Button  
-      _goToPrevProductCard() {
-        var newCurrentPos = 0;
-        if (this._btnPrev.disabled === false) {
-    
-          newCurrentPos = this._currentPos - 1;
-          this._goToProductCard(newCurrentPos, 'prev');
-        }
-      };
-		// create product card and transition
-      _goToProductCard(n, dir) {
-        var direction            = dir ,
-            cardwidth            = this._cards[0].offsetWidth,
-            currentPos           = Math.max(-cardwidth * this._currentPos, this._minPos),
-            scrollWidth          = cardwidth,
-            newPos;
-
-        newPos = direction === 'next' ? Math.max(this._minPos, currentPos - scrollWidth) : Math.min(0, currentPos + scrollWidth);
-		this._cardsContainer.style.transition = 'margin-left ' + this._speed + 'ms' + ' ' + this._effect + ' ' + this._delay + 'ms';
-    	this._cardsContainer.style.marginLeft = newPos + 'px';
-		this._currentPos = n;
-
+      if (this._currentPos === 0) {
+        this._btnPrev.disabled = true;
         this._btnNext.disabled = false;
-        this._btnPrev.disabled = false;
+      }
+    }; // end of function
 
-        
-        if (this._currentPos === this._maxPosIndex) {
-            this._btnNext.disabled = true;
-    		this._btnPrev.disabled = false;
+  } // end of class
 
-        }
-        
-        if (this._currentPos === 0) {
-            this._btnPrev.disabled = true;
-            this._btnNext.disabled = false;
-
-        }
-
-
-      };// end of function
-
-
-
-    } // end of class
 
   function onDocumentReady() {
 
-    const productCarousel = new ProductCarousel();
+    const productCmp = document.querySelectorAll(ProductCarousel.selectors.self);
+    if (productCmp) {
 
+      productCmp.forEach(function(element) {
+        new ProductCarousel(element);
+      });
+
+    }
   }
 
+
   if (document.readyState !== "loading") {
-      onDocumentReady()
+    onDocumentReady()
   } else {
-      document.addEventListener("DOMContentLoaded", onDocumentReady);
+    document.addEventListener("DOMContentLoaded", onDocumentReady);
   }
 
 })();
-
