0eaf9e638b6c17ad1c630ac6d425b8917b5045e7
Deep Kamal Singh <deesingh@adobe.com>
2019-05-28 18:15:02 +0530

    clean up in JS

--- ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/productlist/v1/productlist/clientlibs/dist/js/navhelper.js
+++ ui.apps/src/main/content/jcr_root/apps/venia/components/commerce/productlist/v1/productlist/clientlibs/dist/js/navhelper.js
@@ -1,9 +1,9 @@
 let getCookie = function (cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
@@ -15,16 +15,13 @@ let getCookie = function (cname) {
 };
 
 let setCookie = function (cname, cvalue, exdays) {
-    var d = new Date();
+    let d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
+    let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 };
 
 let switchToPageNumber = function (pageNum) {
 
-    console.log('switchToPageNumber called with pageNum=' + pageNum);
-
-    window.location = "?page="+pageNum;
-
+    window.location = "?page=" + pageNum;
 };
