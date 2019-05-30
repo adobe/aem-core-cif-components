3772565b1fa4aa2df41ec7edd4eb5981c0c8e968
Sandeep Aggarwal <sandagga@adobe.com>
2019-05-31 00:21:54 +0530

    added empty list check for product list

--- bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImplTest.java
@@ -39,59 +39,61 @@ import static org.mockito.Mockito.when;
 import org.mockito.internal.util.reflection.Whitebox;
 
 public class ProductCarouselImplTest {
-    
+
     public static final String PRODUCTLIST = "/graphql/magento-graphql-productcarousel.json";
-    
+
     private ProductCarousel slingModel;
     private List<ProductInterface> productsList;
-    
+
     @Mock
     private String[] productSkuList;
-    
+
     @Before
     public void setUp() throws Exception {
-        
+
         this.slingModel = new ProductCarouselImpl();
-        Whitebox.setInternalState(this.slingModel, "productSkuList",productSkuList);
-        
+        Whitebox.setInternalState(this.slingModel, "productSkuList", productSkuList);
+
         Page productPage = mock(Page.class);
         when(productPage.getLanguage(false)).thenReturn(Locale.US);
         when(productPage.getPath()).thenReturn("/content/test-product-page");
         Whitebox.setInternalState(this.slingModel, "productPage", productPage);
-        
+
         String json = IOUtils.toString(this.getClass()
-            .getResourceAsStream("/graphql/magento-graphql-productcarousel.json"), StandardCharsets.UTF_8);
+                .getResourceAsStream("/graphql/magento-graphql-productcarousel.json"), StandardCharsets.UTF_8);
         Query productQuery = QueryDeserializer.getGson().fromJson(json, Query.class);
-        productsList  = productQuery.getProducts().getItems();
+        productsList = productQuery.getProducts().getItems();
         Whitebox.setInternalState(this.slingModel, "productList", productsList);
     }
 
     @Test
     public void getProducts() {
-        
+
         Collection<ProductListItem> products = this.slingModel.getProducts();
         NumberFormat priceFormatter = NumberFormat.getCurrencyInstance(Locale.US);
-        
-        List<ProductListItem> results = products.stream().collect(Collectors.toList());
-        for (int i = 0; i < results.size(); i++) {
-            
-            ProductInterface productInterface = productsList.get(i);
-            ProductListItem item = results.get(i);
 
-            Assert.assertEquals(productInterface.getName(), item.getTitle());
-            Assert.assertEquals(productInterface.getSku(), item.getSKU());
-            Assert.assertEquals(productInterface.getUrlKey(), item.getSlug());
-            Assert.assertEquals(String.format("/content/test-product-page.%s.html", productInterface.getUrlKey()),
-                item.getURL());
-            Assert.assertEquals(productInterface.getPrice().getRegularPrice().getAmount().getValue(),
-                item.getPrice(), 0);
-            Assert.assertEquals(productInterface.getPrice().getRegularPrice().getAmount().getCurrency().toString(),
-                item.getCurrency());
-            priceFormatter.setCurrency(Currency.getInstance(productInterface.getPrice().getRegularPrice().getAmount().getCurrency().toString()));
-            Assert.assertEquals(priceFormatter.format(productInterface.getPrice().getRegularPrice().getAmount().getValue()),
-                item.getFormattedPrice());
-            Assert.assertTrue(StringUtils.endsWith(item.getImageURL(), productInterface.getThumbnail().getUrl()));
+        List<ProductListItem> results = products.stream().collect(Collectors.toList());
+        if (!results.isEmpty()) {
+            for (int i = 0; i < results.size(); i++) {
+
+                ProductInterface productInterface = productsList.get(i);
+                ProductListItem item = results.get(i);
+
+                Assert.assertEquals(productInterface.getName(), item.getTitle());
+                Assert.assertEquals(productInterface.getSku(), item.getSKU());
+                Assert.assertEquals(productInterface.getUrlKey(), item.getSlug());
+                Assert.assertEquals(String.format("/content/test-product-page.%s.html", productInterface.getUrlKey()),
+                        item.getURL());
+                Assert.assertEquals(productInterface.getPrice().getRegularPrice().getAmount().getValue(),
+                        item.getPrice(), 0);
+                Assert.assertEquals(productInterface.getPrice().getRegularPrice().getAmount().getCurrency().toString(),
+                        item.getCurrency());
+                priceFormatter.setCurrency(Currency.getInstance(productInterface.getPrice().getRegularPrice().getAmount().getCurrency().toString()));
+                Assert.assertEquals(priceFormatter.format(productInterface.getPrice().getRegularPrice().getAmount().getValue()),
+                        item.getFormattedPrice());
+                Assert.assertTrue(StringUtils.endsWith(item.getImageURL(), productInterface.getThumbnail().getUrl()));
+            }
         }
+
     }
 }
-
