c5c8302f3c50f84c275aefcb6e38a13e3663f363
Mark J. Becker <herzog31@users.noreply.github.com>
2019-05-31 15:11:20 +0200

    Finalize product core unit test

--- ui.apps/karma.conf.js
+++ ui.apps/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'src/main/content/jcr_root/apps/venia/components/commerce/product/**/*.js',
-      'src/test/components/commerce/**/*.js'
+      'src/test/components/commerce/**/*Test.js'
     ],
 
 
