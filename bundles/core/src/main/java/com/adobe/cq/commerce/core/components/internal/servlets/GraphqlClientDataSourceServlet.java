2dfdf33d7cf07dc23adbdecd70b5c95ecc9f8e7b
Mark J. Becker <herzog31@users.noreply.github.com>
2019-07-17 12:50:45 +0200

    CIF-756 - Manually configuration of used GraphQL client/catalog (#89)
    * CIF-756 - Add GraphqlClient data source servlet
    
    * CIF-756 - Add inherit option to servlet and storeview input
    
    * CIF-756 - Fix dialog layout with catalogpage
    
    * CIF-756 - Add unit tests for servlet
    
    * CIF-756 - Add feedback
    
    * CIF-756 - Add feedback

---
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/servlets/GraphqlClientDataSourceServlet.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.adobe.cq.commerce.core.components.internal.servlets;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.servlet.Servlet;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.SyntheticResource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.eclipse.jetty.util.ConcurrentHashSet;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+import com.adobe.cq.commerce.graphql.client.GraphqlClient;
+import com.adobe.granite.ui.components.ds.DataSource;
+import com.adobe.granite.ui.components.ds.SimpleDataSource;
+import com.day.cq.i18n.I18n;
+
+@Component(
+    service = { Servlet.class },
+    property = {
+        "sling.servlet.resourceTypes=" + GraphqlClientDataSourceServlet.RESOURCE_TYPE,
+        "sling.servlet.methods=GET",
+        "sling.servlet.extensions=html"
+    })
+public class GraphqlClientDataSourceServlet extends SlingSafeMethodsServlet {
+
+    public final static String RESOURCE_TYPE = "core/cif/components/page/v1/datasource/graphqlclients";
+
+    private I18n i18n;
+
+    private Set<String> identifiers = new ConcurrentHashSet<>();
+
+    @Reference(
+        service = GraphqlClient.class,
+        bind = "bindGraphqlClient",
+        unbind = "unbindGraphqlClient",
+        cardinality = ReferenceCardinality.MULTIPLE,
+        policy = ReferencePolicy.DYNAMIC)
+    void bindGraphqlClient(GraphqlClient graphqlClient, Map<?, ?> properties) {
+        identifiers.add(graphqlClient.getIdentifier());
+    }
+
+    void unbindGraphqlClient(GraphqlClient graphqlClient, Map<?, ?> properties) {
+        identifiers.remove(graphqlClient.getIdentifier());
+    }
+
+    @Override
+    protected void doGet(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response) {
+        i18n = new I18n(request);
+        SimpleDataSource graphqlClientDataSource = new SimpleDataSource(getGraphqlClients(request).iterator());
+        request.setAttribute(DataSource.class.getName(), graphqlClientDataSource);
+    }
+
+    List<Resource> getGraphqlClients(@Nonnull SlingHttpServletRequest request) {
+        ResourceResolver resolver = request.getResourceResolver();
+        List<Resource> graphqlClients = new ArrayList<>();
+
+        // Add empty option
+        graphqlClients.add(new GraphqlClientResource(i18n.get("Inherit", "Inherit property"), StringUtils.EMPTY, resolver));
+
+        // Add other configurations
+        for (String identifier : identifiers) {
+            graphqlClients.add(new GraphqlClientResource(identifier, identifier, resolver));
+        }
+
+        return graphqlClients;
+    }
+
+    protected static class GraphqlClientResource extends SyntheticResource {
+
+        protected static final String PN_VALUE = "value";
+        protected static final String PN_TEXT = "text";
+
+        private String name;
+        private String value;
+        private ValueMap valueMap;
+
+        GraphqlClientResource(String name, String value, ResourceResolver resourceResolver) {
+            super(resourceResolver, StringUtils.EMPTY, RESOURCE_TYPE_NON_EXISTING);
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public <AdapterType> AdapterType adaptTo(Class<AdapterType> type) {
+            if (type == ValueMap.class) {
+                if (valueMap == null) {
+                    initValueMap();
+                }
+                return (AdapterType) valueMap;
+            } else {
+                return super.adaptTo(type);
+            }
+        }
+
+        private void initValueMap() {
+            valueMap = new ValueMapDecorator(new HashMap<String, Object>());
+            valueMap.put(PN_VALUE, getValue());
+            valueMap.put(PN_TEXT, getText());
+        }
+
+        protected String getText() {
+            return name;
+        }
+
+        protected String getValue() {
+            return value;
+        }
+
+        protected boolean getSelected() {
+            return false;
+        }
+    }
+
+}
---
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/servlets/GraphqlClientDataSourceServletTest.java
@@ -0,0 +1,156 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.adobe.cq.commerce.core.components.internal.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.osgi.framework.InvalidSyntaxException;
+
+import com.adobe.cq.commerce.graphql.client.GraphqlClient;
+import com.adobe.granite.ui.components.ds.DataSource;
+import com.day.cq.i18n.I18n;
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
+public class GraphqlClientDataSourceServletTest {
+
+    @Rule
+    public AemContext context = new AemContext();
+
+    private GraphqlClientDataSourceServlet servlet;
+
+    @Before
+    public void setUp() {
+        servlet = new GraphqlClientDataSourceServlet();
+    }
+
+    @Test
+    public void testDataSource() {
+        // Stub out getGraphqlClients call
+        List<Resource> resources = new ArrayList<Resource>();
+        resources.add(new GraphqlClientDataSourceServlet.GraphqlClientResource("my-name", "my-value", null));
+        GraphqlClientDataSourceServlet spyServlet = spy(servlet);
+        Mockito.doReturn(resources).when(spyServlet).getGraphqlClients(any());
+
+        // Test doGet
+        spyServlet.doGet(context.request(), context.response());
+
+        // Verify data source
+        DataSource dataSource = (DataSource) context.request().getAttribute(DataSource.class.getName());
+        Assert.assertNotNull(dataSource);
+
+        AtomicInteger size = new AtomicInteger(0);
+        dataSource.iterator().forEachRemaining(resource -> {
+            GraphqlClientDataSourceServlet.GraphqlClientResource client = (GraphqlClientDataSourceServlet.GraphqlClientResource) resource;
+            Assert.assertEquals("my-name", client.getText());
+            Assert.assertEquals("my-value", client.getValue());
+            Assert.assertFalse(client.getSelected());
+            size.incrementAndGet();
+        });
+        Assert.assertEquals(1, size.get());
+    }
+
+    @Test
+    public void testGetGraphqlClients() throws IOException, InvalidSyntaxException {
+        // Stub i18n
+        I18n i18n = mock(I18n.class);
+        Mockito.doReturn("inherit-translated").when(i18n).get(eq("Inherit"), any());
+        Whitebox.setInternalState(servlet, "i18n", i18n);
+
+        // Add fake identifiers
+        Set<String> identifiers = new HashSet<>();
+        identifiers.add("my-identifier");
+        Whitebox.setInternalState(servlet, "identifiers", identifiers);
+
+        // Call method
+        List<Resource> resources = servlet.getGraphqlClients(context.request());
+
+        // Verify list
+        Assert.assertEquals(2, resources.size());
+
+        // Verify inherit entry
+        GraphqlClientDataSourceServlet.GraphqlClientResource inherit = (GraphqlClientDataSourceServlet.GraphqlClientResource) resources.get(
+            0);
+        Assert.assertEquals("inherit-translated", inherit.getText());
+        Assert.assertEquals("", inherit.getValue());
+        Assert.assertFalse(inherit.getSelected());
+
+        // Verify actual client
+        GraphqlClientDataSourceServlet.GraphqlClientResource client = (GraphqlClientDataSourceServlet.GraphqlClientResource) resources.get(
+            1);
+        Assert.assertEquals("my-identifier", client.getText());
+        Assert.assertEquals("my-identifier", client.getValue());
+        Assert.assertFalse(client.getSelected());
+    }
+
+    @Test
+    public void testGraphqlClientResourceAdapt() {
+        // Create resource
+        GraphqlClientDataSourceServlet.GraphqlClientResource resource = new GraphqlClientDataSourceServlet.GraphqlClientResource("my-name",
+            "my-value", null);
+
+        // Adapt to ValueMap
+        ValueMap map = resource.adaptTo(ValueMap.class);
+
+        // Verify ValueMap
+        Assert.assertEquals("my-name", map.get(GraphqlClientDataSourceServlet.GraphqlClientResource.PN_TEXT));
+        Assert.assertEquals("my-value", map.get(GraphqlClientDataSourceServlet.GraphqlClientResource.PN_VALUE));
+    }
+
+    @Test
+    public void testBindGraphqlClient() {
+        GraphqlClient mockClient = mock(GraphqlClient.class);
+        Mockito.doReturn("my-identifier").when(mockClient).getIdentifier();
+
+        servlet.bindGraphqlClient(mockClient, Collections.emptyMap());
+
+        Set<String> identifiers = (Set<String>) Whitebox.getInternalState(servlet, "identifiers");
+        Assert.assertEquals(1, identifiers.size());
+        Assert.assertTrue(identifiers.contains("my-identifier"));
+    }
+
+    @Test
+    public void testUnbindGraphqlClient() {
+        GraphqlClient mockClient = mock(GraphqlClient.class);
+        Mockito.doReturn("my-identifier").when(mockClient).getIdentifier();
+
+        Set<String> identifiers = new HashSet<>(Arrays.asList("my-identifier", "another-identifier"));
+        Whitebox.setInternalState(servlet, "identifiers", identifiers);
+
+        servlet.unbindGraphqlClient(mockClient, Collections.emptyMap());
+
+        Assert.assertEquals(1, identifiers.size());
+        Assert.assertTrue(identifiers.contains("another-identifier"));
+    }
+}
