f7e8f7fe4053a1a988ae1d47c808271289bd574c
Christophe Jelger <jelger@adobe.com>
2019-08-22 15:37:02 +0200

    CIF-758 - Support for multiple category / product pages
    - minor change

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/servlets/SpecificPageFilterConfiguration.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/servlets/SpecificPageFilterConfiguration.java
@@ -18,7 +18,7 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
-@ObjectClassDefinition(name = "CIF page filter configuration")
+@ObjectClassDefinition(name = "CIF Page Filter Configuration Factory")
 public @interface SpecificPageFilterConfiguration {
 
     @AttributeDefinition(
