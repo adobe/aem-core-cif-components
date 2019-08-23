4b6d17572da0d1b3c6e6aa276733a9408d877528
Daniel Platon <dplaton@pm.me>
2019-08-23 12:59:55 +0300

    @trivial Fix eslint errors

--- ui.apps/src/main/javascript/minicart/src/utils/formValidators.js
+++ ui.apps/src/main/javascript/minicart/src/utils/formValidators.js
@@ -43,7 +43,7 @@ export const isRequired = value => {
 };
 
 export const validateEmail = value => {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return regex.test(value) ? SUCCESS : 'Please enter a valid email address (Ex: johndoe@domain.com).';
 };
