b73a839801a1a319908abb674ae915c25bd5e492
Mark J. Becker <herzog31@users.noreply.github.com>
2019-11-28 13:51:11 +0100

    CIF-995 - Add product customization to product carousel

---
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductsRetriever.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+package com.adobe.cq.commerce.core.components.internal.models.v1.productcarousel;
+
+import com.adobe.cq.commerce.core.components.client.MagentoGraphqlClient;
+import com.adobe.cq.commerce.core.components.models.retriever.AbstractProductsRetriever;
+import com.adobe.cq.commerce.magento.graphql.ProductInterfaceQueryDefinition;
+import com.adobe.cq.commerce.magento.graphql.ProductPricesQueryDefinition;
+import com.adobe.cq.commerce.magento.graphql.SimpleProductQueryDefinition;
+
+class ProductsRetriever extends AbstractProductsRetriever {
+    ProductsRetriever(MagentoGraphqlClient client) {
+        super(client);
+    }
+
+    @Override
+    protected ProductInterfaceQueryDefinition generateProductQuery() {
+        return q -> {
+            q.sku()
+                .name()
+                .thumbnail(t -> t.label()
+                    .url())
+                .urlKey()
+                .price(generatePriceQuery())
+                .onConfigurableProduct(cp -> cp.variants(v -> v.product(generateSimpleProductQuery())));
+
+            // Apply product query hook
+            if (productQueryHook != null) {
+                productQueryHook.accept(q);
+            }
+        };
+    }
+
+    private ProductPricesQueryDefinition generatePriceQuery() {
+        return q -> q.regularPrice(rp -> rp.amount(a -> a.currency().value()));
+    }
+
+    private SimpleProductQueryDefinition generateSimpleProductQuery() {
+        return q -> {
+            q.sku()
+                .name()
+                .thumbnail(t -> t.label()
+                    .url())
+                .price(generatePriceQuery());
+
+            // Apply product variant query hook
+            if (variantQueryHook != null) {
+                variantQueryHook.accept(q);
+            }
+        };
+    }
+}
