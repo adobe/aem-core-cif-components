f94c8d98d39c76e74d1b3d32e7eadac9ab5cec17
Mark J. Becker <herzog31@users.noreply.github.com>
2020-01-30 10:03:57 +0100

    CIF-1226 - Update Product Filters in Components for Magento 2.3.4 Schema

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/models/retriever/AbstractProductsRetriever.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/models/retriever/AbstractProductsRetriever.java
@@ -18,9 +18,9 @@ import java.util.function.Consumer;
 
 import com.adobe.cq.commerce.core.components.client.MagentoGraphqlClient;
 import com.adobe.cq.commerce.graphql.client.GraphqlResponse;
-import com.adobe.cq.commerce.magento.graphql.FilterTypeInput;
+import com.adobe.cq.commerce.magento.graphql.FilterEqualTypeInput;
 import com.adobe.cq.commerce.magento.graphql.Operations;
-import com.adobe.cq.commerce.magento.graphql.ProductFilterInput;
+import com.adobe.cq.commerce.magento.graphql.ProductAttributeFilterInput;
 import com.adobe.cq.commerce.magento.graphql.ProductInterface;
 import com.adobe.cq.commerce.magento.graphql.ProductInterfaceQuery;
 import com.adobe.cq.commerce.magento.graphql.ProductInterfaceQueryDefinition;
@@ -126,8 +126,8 @@ public abstract class AbstractProductsRetriever extends AbstractRetriever {
      * @return GraphQL query as string
      */
     protected String generateQuery(List<String> identifiers) {
-        FilterTypeInput input = new FilterTypeInput().setIn(identifiers);
-        ProductFilterInput filter = new ProductFilterInput().setSku(input);
+        FilterEqualTypeInput skuFilter = new FilterEqualTypeInput().setIn(identifiers);
+        ProductAttributeFilterInput filter = new ProductAttributeFilterInput().setSku(skuFilter);
         QueryQuery.ProductsArgumentsDefinition searchArgs = s -> s.filter(filter);
 
         ProductsQueryDefinition queryArgs = q -> q.items(generateProductQuery());
