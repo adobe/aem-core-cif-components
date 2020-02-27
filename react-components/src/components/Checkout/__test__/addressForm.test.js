1f6d4ffd4edd91f5f5874ddc65218da2ee44a8d2
Mark J. Becker <herzog31@users.noreply.github.com>
2020-02-27 10:26:50 +0100

    CIF-1239 - Entering shipping address does not always work (#211)

---
+++ react-components/src/components/Checkout/__test__/addressForm.test.js
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import AddressForm from '../addressForm';
+
+describe('<AddressForm />', () => {
+    const countries = [
+        {
+            id: 'US',
+            available_regions: [
+                {
+                    name: 'Michigan',
+                    code: 'MI'
+                }
+            ]
+        }
+    ];
+
+    it('renders the component', () => {
+        const { asFragment } = render(<AddressForm cancel={() => {}} submit={() => {}} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('fills the street0 field with a given street array value', () => {
+        const initialValues = {
+            street: ['street A', 'street B']
+        };
+
+        const { container } = render(<AddressForm cancel={() => {}} submit={() => {}} initialValues={initialValues} />);
+
+        const streetField = container.querySelector('#street0');
+        expect(streetField.value).toEqual('street A');
+    });
+
+    it('returns the street field as array', () => {
+        const initialValues = {
+            country_id: 'US',
+            firstname: 'Veronica',
+            lastname: 'Costello',
+            city: 'Calder',
+            postcode: '49628-7978',
+            region_id: 33,
+            region_code: 'MI',
+            region: 'Michigan',
+            telephone: '(555) 229-3326',
+            email: 'veronica@example.com'
+        };
+
+        const mockSubmit = jest.fn(() => {});
+        const { container } = render(
+            <AddressForm cancel={() => {}} submit={mockSubmit} initialValues={initialValues} countries={countries} />
+        );
+
+        // Fill street input
+        const streetField = container.querySelector('#street0');
+        fireEvent.change(streetField, { target: { value: 'street A' } });
+
+        // Click on submit
+        let submit = container.querySelector('button[type=submit]');
+        fireEvent.click(submit);
+
+        // Verify parameter
+        expect(mockSubmit).toHaveBeenCalledTimes(1);
+        const submitValues = mockSubmit.mock.calls[0][0];
+        expect(submitValues.street).toEqual(['street A']);
+    });
+});
