adobe/aem-core-cif-components

commit 27755ce1ccd33cdb3aef37126a9fd34ef7b58371

CIF-1253 - Product Carousel does not show configurable product and pr… (#202)

bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImpl.java
@@ -112,18 +112,17 @@ public class ProductCarouselImpl implements ProductCarousel {
 
     @Override
     public List<ProductListItem> getProducts() {
-        List<ProductInterface> productList = productsRetriever.fetchProducts();
-        Collections.sort(productList, Comparator.comparing(item -> baseProductSkus.indexOf(item.getSku())));
+        List<ProductInterface> products = productsRetriever.fetchProducts();
+        Collections.sort(products, Comparator.comparing(item -> baseProductSkus.indexOf(item.getSku())));
 
         List<ProductListItem> carouselProductList = new ArrayList<>();
-        if (!productList.isEmpty()) {
-            for (ProductInterface product : productList) {
-                // Find the baseProductSku that was used to fetch that product
-                int idx = baseProductSkus.indexOf(product.getSku());
-
-                // We know the list of skus is in the same order as the list of products
-                // but we searched the index because a product might not have been found (see unit test)
-                Pair<String, String> skus = SiteNavigation.toProductSkus(productSkuList[idx]);
+        if (!products.isEmpty()) {
+            for (String combinedSku : productSkuList) {
+                Pair<String, String> skus = SiteNavigation.toProductSkus(combinedSku);
+                ProductInterface product = products.stream().filter(p -> p.getSku().equals(skus.getLeft())).findFirst().orElse(null);
+                if (product == null) {
+                    continue; // Can happen that a product is not found
+                }
 
                 String slug = product.getUrlKey();
                 if (skus.getRight() != null && product instanceof ConfigurableProduct) {

bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImplTest.java
@@ -104,19 +104,21 @@ public class ProductCarouselImplTest {
     public void getProducts() {
 
         List<ProductListItem> items = productCarousel.getProducts();
-        Assert.assertFalse(items.isEmpty());
+        Assert.assertEquals(4, items.size()); // one product is not found
 
         List<String> productSkuList = Arrays.asList(productSkuArray);
         NumberFormat priceFormatter = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (int i = 0; i < items.size(); i++) {
-            ProductListItem item = items.get(i);
-            ProductInterface product = products.get(i);
-
-            // Find the combinedSku that was used to fetch that product
-            String combinedSku = productSkuList.stream().filter(s -> s.contains(item.getSKU())).findFirst().orElse(null);
+        int idx = 0;
+        for (String combinedSku : productSkuList) {
             Pair<String, String> skus = SiteNavigation.toProductSkus(combinedSku);
+            ProductInterface product = products.stream().filter(p -> p.getSku().equals(skus.getLeft())).findFirst().orElse(null);
+            if (product == null) {
+                continue; // Can happen that a product is not found
+            }
+
             ProductInterface productOrVariant = toProductOrVariant(product, skus);
+            ProductListItem item = items.get(idx);
 
             Assert.assertEquals(productOrVariant.getName(), item.getTitle());
             Assert.assertEquals(product.getSku(), item.getSKU());
@@ -133,6 +135,7 @@ public class ProductCarouselImplTest {
             Assert.assertEquals(priceFormatter.format(amount.getValue()), item.getFormattedPrice());
 
             Assert.assertEquals(productOrVariant.getThumbnail().getUrl(), item.getImageURL());
+            idx++;
         }
     }
 

commit bd3d239aef6c7d22ffbe06ab26ee9413add81ef1

CIF-1313 - Product carousel no longer displays product configured with JCR path
- fixed regression bug introduced by #202
- extended unit test to make sure this is properly covered

bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImpl.java
@@ -123,6 +123,11 @@ public class ProductCarouselImpl implements ProductCarousel {
         List<ProductListItem> carouselProductList = new ArrayList<>();
         if (!products.isEmpty()) {
             for (String combinedSku : productSkuList) {
+
+                if (combinedSku.startsWith("/")) {
+                    combinedSku = StringUtils.substringAfterLast(combinedSku, "/");
+                }
+
                 Pair<String, String> skus = SiteNavigation.toProductSkus(combinedSku);
                 ProductInterface product = products.stream().filter(p -> p.getSku().equals(skus.getLeft())).findFirst().orElse(null);
                 if (product == null) {

bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/models/v1/productcarousel/ProductCarouselImplTest.java
@@ -19,7 +19,9 @@ import java.util.Arrays;
 import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.scripting.SlingBindings;
@@ -96,7 +98,11 @@ public class ProductCarouselImplTest {
         List<ProductListItem> items = productCarousel.getProducts();
         Assert.assertEquals(4, items.size()); // one product is not found and the JSON response contains a "faulty" product
 
-        List<String> productSkuList = Arrays.asList(productSkuArray);
+        List<String> productSkuList = Arrays.asList(productSkuArray)
+            .stream()
+            .map(s -> s.startsWith("/") ? StringUtils.substringAfterLast(s, "/") : s)
+            .collect(Collectors.toList());
+
         NumberFormat priceFormatter = NumberFormat.getCurrencyInstance(Locale.US);
 
         int idx = 0;

bundles/core/src/test/resources/test-queries/graphql-requests.log
@@ -18,5 +18,5 @@
 
 {category(id:6){id,description,name,image,product_count,products(currentPage:1,pageSize:6){items{__typename,id,sku,name,small_image{url},url_key,price_range{minimum_price{regular_price{value,currency},final_price{value,currency},discount{amount_off,percent_off}}},... on ConfigurableProduct{price_range{maximum_price{regular_price{value,currency},final_price{value,currency},discount{amount_off,percent_off}}}}},total_count}}}
 
-{category__category_5:category(id:5){id,name,url_path,position,image},category__category_6:category(id:6){id,name,url_path,position,image},category__category_7:category(id:7){id,name,url_path,position,image}}
+{category__category_5:category(id:5){id,name,url_path,position,image},category__category_6:category(id:6){id,name,url_path,position,image},category__category_7:category(id:7){id,name,url_path,position,image},category__category_9999:category(id:9999){id,name,url_path,position,image}}
 