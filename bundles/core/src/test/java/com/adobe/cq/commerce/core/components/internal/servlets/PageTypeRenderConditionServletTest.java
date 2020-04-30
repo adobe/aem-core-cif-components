d248b34a40e28086640c10c889deb20c22ca188f
Levente Sántha <sntha@adobe.com>
2020-04-30 16:47:56 +0300

    CIF-1321 - Empty Commerce tab in page properties (#256)
    * added commerce tab in page properties with appropriate render conditions for custom PDPs and PLPs
    * added new render condtion to avoid using page templates in render conditions for page properties of custom PDPs and PLPs
    * removed old render condition and related unit test

---
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/servlets/PageTypeRenderConditionServletTest.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ *
+ *    Copyright 2020 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.adobe.cq.commerce.core.components.internal.servlets;
+
+import java.util.HashMap;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.wrappers.ModifiableValueMapDecorator;
+import org.apache.sling.servlethelpers.MockSlingHttpServletRequest;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.adobe.granite.ui.components.rendercondition.RenderCondition;
+import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import io.wcm.testing.mock.aem.junit.AemContextCallback;
+
+public class PageTypeRenderConditionServletTest {
+    @Rule
+    public final AemContext context = createContext("/context/jcr-page-filter.json");
+
+    private static AemContext createContext(String contentPath) {
+        return new AemContext(
+            (AemContextCallback) context -> {
+                context.load().json(contentPath, "/content");
+            },
+            ResourceResolverType.JCR_MOCK);
+    }
+
+    private MockSlingHttpServletRequest request;
+    private PageTypeRenderConditionServlet servlet;
+
+    @Before
+    public void setUp() {
+        request = new MockSlingHttpServletRequest(context.resourceResolver());
+        Resource resource = Mockito.mock(Resource.class);
+        Mockito.when(resource.getPath()).thenReturn("/here");
+        Mockito.when(resource.getResourceResolver()).thenReturn(context.resourceResolver());
+        Mockito.when(resource.getValueMap()).thenReturn(new ModifiableValueMapDecorator(new HashMap<>()));
+        request.setResource(resource);
+        servlet = new PageTypeRenderConditionServlet();
+    }
+
+    @Test
+    public void testProductPage() {
+        request.getResource().getValueMap().put("pageType", "product");
+        request.setQueryString("item=/content/product-page/sub-page");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertTrue(condition.check());
+    }
+
+    @Test
+    public void testCategoryPage() {
+        request.getResource().getValueMap().put("pageType", "category");
+        request.setQueryString("item=/content/category-page/sub-page");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertTrue(condition.check());
+    }
+
+    @Test
+    public void testNotProductPage() {
+        request.getResource().getValueMap().put("pageType", "product");
+        request.setQueryString("item=/content/category-page/sub-page");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertFalse(condition.check());
+    }
+
+    @Test
+    public void testNotCategoryPage() {
+        request.getResource().getValueMap().put("pageType", "category");
+        request.setQueryString("item=/content/product-page/sub-page");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertFalse(condition.check());
+    }
+
+    @Test
+    public void testNotPage() {
+        request.getResource().getValueMap().put("pageType", "product");
+        request.setQueryString("item=/content/product-page/ignored");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertFalse(condition.check());
+    }
+
+    @Test
+    public void testNoPageType() {
+        request.setQueryString("item=/content/product-page/ignored");
+        request.setPathInfo("/mnt/overlay/wcm/core/content/sites/properties.html");
+
+        servlet.doGet(request, null);
+
+        SimpleRenderCondition condition = (SimpleRenderCondition) request.getAttribute(RenderCondition.class.getName());
+        Assert.assertFalse(condition.check());
+    }
+}
