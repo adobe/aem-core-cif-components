1950b127243b3a230c49a8ba6211cae2c82a153d
Laurentiu Magureanu <magurean@adobe.com>
2020-06-17 14:24:38 +0300

    CIF-1437: Fixed Content teaser editor bug:  - Disable category input when actions get disabled  - Allow only one action target (category, product) to be filled

---
+++ ui.apps/src/main/content/jcr_root/apps/core/cif/components/content/teaser/v1/teaser/clientlib/editor/js/teaser.js
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ *
+ *    Copyright 2020 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+(($) => {
+    "use strict";
+
+    const dialogContentSelector = '[data-cmp-is="commerceteaser-editor"].cmp-teaser__editor';
+    const productFieldSelector = '[data-cmp-teaser-v1-dialog-edit-hook="actionLink"][placeholder="Product"]';
+    const categoryFieldSelector = '[data-cmp-teaser-v1-dialog-edit-hook="actionLink"][placeholder="Category"]';
+    const actionsMultifieldSelector = '.cmp-teaser__editor-multifield_actions';
+    const actionsMultifieldItemSelector = '.coral3-Multifield-item';
+    const actionsEnabledCheckboxSelector = 'coral-checkbox[name="./actionsEnabled"]';
+
+    $(document).on("dialog-loaded", e => {
+        const $dialog = e.dialog;
+        const $dialogContent = $dialog.find(dialogContentSelector);
+        const dialogContent = $dialogContent.length > 0 ? $dialogContent[0] : undefined;
+
+        // check if the expected dialog was loaded
+        if (dialogContent) {
+            const multiFieldActions = $dialogContent.find(actionsMultifieldSelector)[0];
+
+            // Handle pickers values change for existing actions
+            handlePickersChange(multiFieldActions);
+
+            // Add change handler to multifield actions
+            multiFieldActions.on('change', () => {
+                // whenever actions are being added/removed, reattach picker change handlers
+                handlePickersChange(multiFieldActions);
+            })
+
+            // Fix Core WCM Components Teaser editor bug.
+            // when actions are disabled, only products picker gets disabled ( Core WCM Components Teaser expects only one action )
+            const $actionsEnabledCheckbox = $dialogContent.find(actionsEnabledCheckboxSelector);
+            $actionsEnabledCheckbox.on("change", e => {
+                const actionsEnabled = $(e.target).adaptTo("foundation-field").getValue() === "true";
+                $(categoryFieldSelector).each((ix, catEl) => {
+                    $(catEl).adaptTo("foundation-field").setDisabled(!actionsEnabled);
+                })
+            });
+        }
+    });
+
+    // used to handle picker value changes and keep only one picker populated
+    const handlePickersChange = multiFieldActions => {
+        $(multiFieldActions).find(actionsMultifieldItemSelector).each((ix, action) => {
+            const productElement = $(action).find(productFieldSelector);
+            const categoryElement = $(action).find(categoryFieldSelector);
+            const productField = productElement.adaptTo("foundation-field");
+            const categoryField = categoryElement.adaptTo("foundation-field");
+
+            // remove attached handlers
+            productElement.off('change', handleProductChange);
+            categoryElement.off('change', handleCategoryChange);
+
+            const eventData = {productField, categoryField};
+            // [re]attach change handlers
+            productElement.on('change', eventData, handleProductChange)
+            categoryElement.on('change', eventData, handleCategoryChange)
+        })
+    }
+
+    // sets an empty value on the category field when product field gets updated
+    const handleProductChange = ({data: {productField, categoryField}}) => {
+        if (productField.getValue() !== "") {
+            categoryField.setValue("")
+        }
+    }
+
+    // sets an empty value on the product field when category field gets updated
+    const handleCategoryChange = ({data: {productField, categoryField}}) => {
+        if (categoryField.getValue() !== "") {
+            productField.setValue("")
+        }
+    }
+})(jQuery);
