9bb7d3d064fa2ae89beb4b86759132ec8b90f31d
Laurentiu Magureanu <magurean@adobe.com>
2020-06-18 17:56:39 +0300

    CIF-1427: Change logo URL

--- react-components/src/components/LoadingIndicator/indicator.js
+++ react-components/src/components/LoadingIndicator/indicator.js
@@ -22,7 +22,7 @@ const LoadingIndicator = props => {
         <div className={className}>
             <img
                 className={classes.indicator}
-                src="/conf/venia/settings/wcm/templates/product-page/structure/_jcr_content/root/header/logo.svg"
+                src="/etc.clientlibs/venia/clientlibs/theme/resources/logo.svg"
                 width="64"
                 height="64"
                 alt="Loading indicator"
