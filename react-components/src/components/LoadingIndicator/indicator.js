007cc44ab373eac5882c241a34f29ccbea1e9feb
laurentiumagureanu <magurean@adobe.com>
2020-06-24 12:07:40 +0300

    Load Venia loading indicator (logo.svg) via CSS (#312)
    * CIF-1427: Change logo URL
    
    * CIF-1427: Change loader img to span with bakground
    
    Co-authored-by: Mark J. Becker <herzog31@users.noreply.github.com>

--- react-components/src/components/LoadingIndicator/indicator.js
+++ react-components/src/components/LoadingIndicator/indicator.js
@@ -20,13 +20,7 @@ const LoadingIndicator = props => {
 
     return (
         <div className={className}>
-            <img
-                className={classes.indicator}
-                src="/conf/venia/settings/wcm/templates/product-page/structure/_jcr_content/root/header/logo.svg"
-                width="64"
-                height="64"
-                alt="Loading indicator"
-            />
+            <span className={`${classes.indicator} loader-img`}></span>
             <span className={classes.message}>{props.children}</span>
         </div>
     );
