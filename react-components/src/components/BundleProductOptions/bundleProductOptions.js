e5f077501ea64001f1001e42200517e2477317b3
Laurentiu Magureanu <magurean@adobe.com>
2020-08-17 14:18:29 +0300

    @trivial: fix formatting

--- react-components/src/components/BundleProductOptions/bundleProductOptions.js
+++ react-components/src/components/BundleProductOptions/bundleProductOptions.js
@@ -179,76 +179,76 @@ const BundleProductOptions = props => {
                         </Button>
                     </section>
                 ) : (
-                        <>
-                            {bundleState.options.items.map(e => (
-                                <section key={`item-${e.option_id}`} className={'productFullDetail__section '}>
-                                    <h3 className="option__title">
-                                        <span>{e.title}</span>{' '}
-                                        {e.required && <span className={classes.required_info}> *</span>}
-                                    </h3>
-                                    <div>{renderItemOptions(e)}</div>
-                                </section>
-                            ))}
-                            <section className="productFullDetail__section">
-                                <h3>
-                                    <span className={classes.required_info}>
-                                        * {t('product:required-fields', 'Required fields')}
-                                    </span>
-                                    <span className={classes.customization__info}>
-                                        {t('product:customization-price', 'Your customization')}: {getTotalPrice()}
-                                    </span>
+                    <>
+                        {bundleState.options.items.map(e => (
+                            <section key={`item-${e.option_id}`} className={'productFullDetail__section '}>
+                                <h3 className="option__title">
+                                    <span>{e.title}</span>{' '}
+                                    {e.required && <span className={classes.required_info}> *</span>}
                                 </h3>
+                                <div>{renderItemOptions(e)}</div>
                             </section>
-                            <section className="productFullDetail__quantity productFullDetail__section">
-                                <h2 className="productFullDetail__quantityTitle option__title">
-                                    <span>{t('cart:quantity', 'Quantity')}</span>
-                                </h2>
-                                <div className="quantity__root">
-                                    <span className={'fieldIcons__root ' + classes.select_icons}>
-                                        <span className="fieldIcons__input">
-                                            <select
-                                                aria-label="product's quantity"
-                                                className="select__input field__input"
-                                                name="quantity">
-                                                <option value="1">1</option>
-                                                <option value="2">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                            </select>
-                                        </span>
-                                        <span className="fieldIcons__before"></span>
-                                        <span className="fieldIcons__after">
-                                            <span className="icon__root">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="18"
-                                                    height="18"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round">
-                                                    <polyline points="6 9 12 15 18 9"></polyline>
-                                                </svg>
-                                            </span>
+                        ))}
+                        <section className="productFullDetail__section">
+                            <h3>
+                                <span className={classes.required_info}>
+                                    * {t('product:required-fields', 'Required fields')}
+                                </span>
+                                <span className={classes.customization__info}>
+                                    {t('product:customization-price', 'Your customization')}: {getTotalPrice()}
+                                </span>
+                            </h3>
+                        </section>
+                        <section className="productFullDetail__quantity productFullDetail__section">
+                            <h2 className="productFullDetail__quantityTitle option__title">
+                                <span>{t('cart:quantity', 'Quantity')}</span>
+                            </h2>
+                            <div className="quantity__root">
+                                <span className={'fieldIcons__root ' + classes.select_icons}>
+                                    <span className="fieldIcons__input">
+                                        <select
+                                            aria-label="product's quantity"
+                                            className="select__input field__input"
+                                            name="quantity">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                        </select>
+                                    </span>
+                                    <span className="fieldIcons__before"></span>
+                                    <span className="fieldIcons__after">
+                                        <span className="icon__root">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="18"
+                                                height="18"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round">
+                                                <polyline points="6 9 12 15 18 9"></polyline>
+                                            </svg>
                                         </span>
                                     </span>
-                                    <p className="message-root"></p>
-                                </div>
-                            </section>
-                            <section className="productFullDetail__cartActions productFullDetail__section">
-                                <button
-                                    className="button__root_highPriority button__root clickable__root button__filled"
-                                    type="button"
-                                    disabled="">
-                                    <span className="button__content">
-                                        <span>{t('product:add-item', 'Add to Cart')}</span>
-                                    </span>
-                                </button>
-                            </section>
-                        </>
-                    )}
+                                </span>
+                                <p className="message-root"></p>
+                            </div>
+                        </section>
+                        <section className="productFullDetail__cartActions productFullDetail__section">
+                            <button
+                                className="button__root_highPriority button__root clickable__root button__filled"
+                                type="button"
+                                disabled="">
+                                <span className="button__content">
+                                    <span>{t('product:add-item', 'Add to Cart')}</span>
+                                </span>
+                            </button>
+                        </section>
+                    </>
+                )}
             </>
         );
     }
