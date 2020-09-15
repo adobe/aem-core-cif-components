60385f5f0af8ea8edc01013b6e913e3cdf6dab4c
hbacila <53909563+hbacila@users.noreply.github.com>
2020-09-15 16:22:20 +0100

    CIF-1624: Remove commons-text dependency in UrlProviderImpl (#396)
    * added local StringSubstitutor drop-in replacement implementation
    * removed commons-text dependency
    * added unit tests

--- bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/services/UrlProviderImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/services/UrlProviderImplTest.java
@@ -14,6 +14,7 @@
 
 package com.adobe.cq.commerce.core.components.internal.services;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -185,5 +186,23 @@ public class UrlProviderImplTest {
         Pair<ProductIdentifierType, String> id = urlProvider.getProductIdentifier(context.request());
         Assert.assertEquals(ProductIdentifierType.SKU, id.getLeft());
         Assert.assertEquals("MJ01", id.getRight());
+    }
+
+    @Test
+    public void testStringSubstitutor() {
+        Map<String, String> params = new HashMap<>();
+
+        // empty params, valid prefix & suffix
+        UrlProviderImpl.StringSubstitutor sub = new UrlProviderImpl.StringSubstitutor(params, "${", "}");
+        Assert.assertEquals("Wrong substitution", "${test}", sub.replace("${test}"));
+
+        // valid params, no prefix & suffix
+        params.put("test", "value");
+        sub = new UrlProviderImpl.StringSubstitutor(params, null, null);
+        Assert.assertEquals("Wrong substitution", "${value}-value", sub.replace("${test}-test"));
+
+        // valid params, prefix & suffix
+        sub = new UrlProviderImpl.StringSubstitutor(params, "${", "}");
+        Assert.assertEquals("Wrong substitution", "value-value", sub.replace("${test}-${test}"));
     }
 }
