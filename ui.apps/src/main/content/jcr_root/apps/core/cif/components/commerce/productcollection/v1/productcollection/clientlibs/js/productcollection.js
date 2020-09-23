794843120069d015c38c283f68172f765dca169b
Christophe Jelger <jelger@adobe.com>
2020-09-23 16:58:38 +0200

    CIF-1609 - Lazy Loading for PLP
    - add unit tests

--- ui.apps/src/main/content/jcr_root/apps/core/cif/components/commerce/productcollection/v1/productcollection/clientlibs/js/productcollection.js
+++ ui.apps/src/main/content/jcr_root/apps/core/cif/components/commerce/productcollection/v1/productcollection/clientlibs/js/productcollection.js
@@ -22,7 +22,7 @@ class ProductCollection {
             sortKeySelect.addEventListener('change', () => this._applySortKey(sortKeySelect));
         }
 
-        let loadMoreButton = document.querySelector(ProductCollection.selectors.loadMoreButton);
+        let loadMoreButton = this._element.querySelector(ProductCollection.selectors.loadMoreButton);
         if (loadMoreButton) {
             loadMoreButton.addEventListener('click', () => this._loadMore(loadMoreButton));
         }
@@ -159,38 +159,40 @@ class ProductCollection {
     _applySortKey(sortKeySelect) {
         window.location = sortKeySelect.options[sortKeySelect.selectedIndex].value;
     }
-    
+
+    async _fetchMoreProducts(loadMoreButton) {
+        let url = loadMoreButton.dataset.loadMore;
+        return fetch(url);
+    }
+
     async _loadMore(loadMoreButton) {
-        
         // Hide load more button and show spinner
         loadMoreButton.style.display = 'none';
-        let spinner = document.querySelector(ProductCollection.selectors.loadMoreSpinner);
+        let spinner = this._element.querySelector(ProductCollection.selectors.loadMoreSpinner);
         spinner.style.display = 'block';
-        
-        let url = loadMoreButton.dataset.loadMore;
-        console.log('Loading more from ' + url);
-        let response = await fetch(url);
+
+        let response = await this._fetchMoreProducts(loadMoreButton);
         if (!response.ok || response.errors) {
             // The query failed, we show the button again
             loadMoreButton.style.display = 'block';
             let message = await response.text();
             throw new Error(message);
         }
-        
+
         // Delete load more button and hide spinner
         loadMoreButton.parentNode.removeChild(loadMoreButton);
         spinner.style.display = 'none';
-        
+
         // Parse response and only select product items
         let text = await response.text();
         let domParser = new DOMParser();
-        let more = domParser.parseFromString(text, "text/html");
+        let more = domParser.parseFromString(text, 'text/html');
         let moreItems = more.querySelectorAll(ProductCollection.selectors.item);
-        
+
         // Append new product items to existing product gallery
-        let galleryItems = document.querySelector(ProductCollection.selectors.galleryItems);
+        let galleryItems = this._element.querySelector(ProductCollection.selectors.galleryItems);
         galleryItems.append(...moreItems);
-        
+
         // If any, append the new "load more" button
         let newloadMoreButton = more.querySelector(ProductCollection.selectors.loadMoreButton);
         if (newloadMoreButton) {
