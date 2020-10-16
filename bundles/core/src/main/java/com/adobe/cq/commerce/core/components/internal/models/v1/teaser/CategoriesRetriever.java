3d38d1e175549dcbf9c241ea0bfa75ae3d3cb4b8
Markus Haack <mhaack@adobe.com>
2020-10-16 11:40:51 +0200

    CIF-1645 - Teaser component must retrieve category url_path (#421)
    * CIF-1645 - Teaser component must retrieve category url_path for correct SEO URL rendering
    
    * CIF-1645 - Improve test coverage
    
    * CIF-1645 - Remove unused logger
    
    Co-authored-by: Christophe Jelger <jelger@adobe.com>

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/teaser/CategoriesRetriever.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/teaser/CategoriesRetriever.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- *    Copyright 2019 Adobe. All rights reserved.
+ *    Copyright 2020 Adobe. All rights reserved.
  *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License. You may obtain a copy
  *    of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -11,32 +11,28 @@
  *    governing permissions and limitations under the License.
  *
  ******************************************************************************/
-
 package com.adobe.cq.commerce.core.components.internal.models.v1.teaser;
 
-import javax.annotation.Nonnull;
+import com.adobe.cq.commerce.core.components.client.MagentoGraphqlClient;
+import com.adobe.cq.commerce.core.components.models.retriever.AbstractCategoriesRetriever;
+import com.adobe.cq.commerce.magento.graphql.CategoryTreeQueryDefinition;
 
-import com.adobe.cq.wcm.core.components.models.ListItem;
+public class CategoriesRetriever extends AbstractCategoriesRetriever {
 
-public class CommerceTeaserActionItem implements ListItem {
-
-    private String title;
-    private String url;
-
-    public CommerceTeaserActionItem(String title, String url) {
-        this.title = title;
-        this.url = url;
+    CategoriesRetriever(MagentoGraphqlClient client) {
+        super(client);
     }
 
-    @Nonnull
     @Override
-    public String getTitle() {
-        return title;
-    }
+    protected CategoryTreeQueryDefinition generateCategoryQuery() {
+        CategoryTreeQueryDefinition categoryTreeQueryDefinition = q -> {
+            q.id().urlPath();
 
-    @Nonnull
-    @Override
-    public String getURL() {
-        return url;
+            if (categoryQueryHook != null) {
+                categoryQueryHook.accept(q);
+            }
+        };
+
+        return categoryTreeQueryDefinition;
     }
 }
