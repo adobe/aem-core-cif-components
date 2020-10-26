9dc7d1295851f0b117b8e8bc0295c4f610756b8a
Daniel Platon <platon@adobe.com>
2020-10-26 12:18:21 +0200

    CIF-1672 - [CIF components library] ui.frontend should use npm link
    Configure the `fedDev` plugin to also build the `React Components` package and link it so it can be used at build time.

--- examples/ui.frontend/webpack.common.js
+++ examples/ui.frontend/webpack.common.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const path                    = require('path');
-const webpack                 = require('webpack');
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
-const TSConfigPathsPlugin     = require('tsconfig-paths-webpack-plugin');
-const CopyWebpackPlugin       = require('copy-webpack-plugin');
-const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const SOURCE_ROOT = __dirname + '/src/main/webpack';
 
 module.exports = {
     resolve: {
         extensions: ['.js', '.ts'],
-        plugins: [new TSConfigPathsPlugin({
-            configFile: './tsconfig.json'
-        })]
+        plugins: [
+            new TSConfigPathsPlugin({
+                configFile: './tsconfig.json',
+            }),
+        ],
     },
     entry: {
-        site: SOURCE_ROOT + '/site/main.ts'
+        site: SOURCE_ROOT + '/site/main.ts',
     },
     output: {
         filename: 'cif-examples-react/[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
@@ -36,20 +38,20 @@ module.exports = {
                         loader: require.resolve('eslint-loader'),
                     },
                     {
-                        loader: 'ts-loader'
+                        loader: 'ts-loader',
                     },
                     {
                         loader: 'webpack-import-glob-loader',
                         options: {
-                            url: false
-                        }
-                    }
-                ]
+                            url: false,
+                        },
+                    },
+                ],
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: ['babel-loader', 'eslint-loader']
+                loader: ['babel-loader', 'eslint-loader'],
             },
             {
                 test: /\.scss$/,
@@ -58,47 +60,48 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: {
-                            url: false
-                        }
+                            url: false,
+                        },
                     },
                     {
                         loader: 'postcss-loader',
                         options: {
                             plugins() {
-                                return [
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
+                                return [require('autoprefixer')];
+                            },
+                        },
                     },
                     {
                         loader: 'sass-loader',
                         options: {
-                            url: false
-                        }
+                            url: false,
+                        },
                     },
                     {
                         loader: 'webpack-import-glob-loader',
                         options: {
-                            url: false
-                        }
-                    }
-                ]
-            }
-        ]
+                            url: false,
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'cif-examples-react/[name].css'
+            filename: 'cif-examples-react/[name].css',
         }),
         new CopyWebpackPlugin([
             {
-                from: path.resolve(__dirname, 'node_modules/@adobe/aem-core-cif-react-components/i18n'),
-                to: './cif-examples-react/i18n'
-            }
-        ])
+                from: path.resolve(
+                    __dirname,
+                    'node_modules/@adobe/aem-core-cif-react-components/i18n'
+                ),
+                to: './cif-examples-react/i18n',
+            },
+        ]),
     ],
     stats: {
         assetsSort: 'chunks',
@@ -114,6 +117,6 @@ module.exports = {
         performance: true,
         providedExports: false,
         source: false,
-        warnings: true
-    }
+        warnings: true,
+    },
 };
--- examples/ui.frontend/webpack.dev.js
+++ examples/ui.frontend/webpack.dev.js
@@ -1,30 +1,10 @@
-const merge             = require('webpack-merge');
-const common            = require('./webpack.common.js');
-const path              = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-const SOURCE_ROOT = __dirname + '/src/main/webpack';
-
-module.exports = env => {
-    const writeToDisk = env && Boolean(env.writeToDisk);
-
+module.exports = (env) => {
     return merge(common, {
         mode: 'development',
         devtool: 'inline-source-map',
-        performance: { hints: 'warning' },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, SOURCE_ROOT + '/static/index.html')
-            })
-        ],
-        devServer: {
-            inline: true,
-            proxy: [{
-                context: ['/content', '/etc.clientlibs'],
-                target: 'http://localhost:4502',
-            }],
-            writeToDisk,
-            liveReload: !writeToDisk
-        }
+        performance: {hints: 'warning'},
     });
-}
+};
