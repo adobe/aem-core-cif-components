4e1681dd73f98b02ffef9e0a11088a0303d0c153
Levente Sántha <levente@adobe.com>
2020-10-26 12:28:01 +0200

    CIF-1608 - Category Carousel Component (#417)
    * CIF-1608 - Category Carousel Component
    
     * created generic carousel component
     * created category carousel component
     * adjusted product carousel and related products component to extend the generic carousel component
     * added unit tests
    
    * CIF-1608 - Category Carousel Component
    
     * updated examples project with category carousel
     * added integration test for category carousel
    
    * CIF-1608 - Category Carousel Component
    
     * fixed IT failure
    
    * CIF-1608 - Category Carousel Component
    
     * fixed category carousel component icon in the component palette of page editor
    
    * CIF-1608 - Category Carousel Component
    
     * fixed code after merge conflicts
    
    * CIF-1608 - Category Carousel Component
    
     * extracted card template in featured category list
    
    * CIF-1608 - Category Carousel Component
    
     * clarifications in generic carousel
    
    * CIF-1608 - Category Carousel Component
    
     * fixed issue with JS code duplication
    
    Co-authored-by: Alex Kim <69959652+alkim91@users.noreply.github.com>

--- ui.apps/src/main/content/jcr_root/apps/core/cif/components/commerce/productcarousel/v1/productcarousel/clientlibs/js/product-carousel.js
+++ ui.apps/src/main/content/jcr_root/apps/core/cif/components/commerce/productcarousel/v1/productcarousel/clientlibs/js/product-carousel.js
@@ -11,131 +11,24 @@
  *    governing permissions and limitations under the License.
  *
  ******************************************************************************/
+'use strict';
 
 (function() {
-    selectors = {
+    const selectors = {
         self: "[data-comp-is='productcarousel']",
         btnPrev: "[data-carousel-action='prev']",
-        btnNext: "[data-carousel-action='next']"
+        btnNext: "[data-carousel-action='next']",
+        container: '.productcarousel__cardscontainer',
+        root: '.productcarousel__root',
+        parent: '.productcarousel__parent',
+        card: '.product__card'
     };
-
-    /**
-     *  Product Carousel Component
-     */
-    class ProductCarousel {
-        constructor(rootElement) {
-            this._cardsContainer = rootElement.querySelector('.productcarousel__cardscontainer');
-            if (!this._cardsContainer) {
-                // the carousel is empty
-                return;
-            }
-
-            // Re-calculate carousel state when the window size changes
-            this._calculate = this._calculate.bind(this);
-            window.addEventListener('resize', this._calculate);
-
-            this._speed = 300;
-            this._delay = 0;
-            this._effect = 'linear';
-            this._productcarousel_root = rootElement.querySelector('.productcarousel__root');
-            this._currentRootWidth = 0;
-            this._product_carousel_parent = rootElement.querySelector('.productcarousel__parent');
-            this._cards = this._cardsContainer.querySelectorAll('.product__card');
-            this._btnPrev = rootElement.querySelector(selectors.btnPrev);
-            this._btnNext = rootElement.querySelector(selectors.btnNext);
-            this._currentPos = 0;
-
-            this._calculate();
-
-            this._btnPrev.addEventListener('click', e => this._goToPrevProductCard());
-            this._btnNext.addEventListener('click', e => this._goToNextProductCard());
-        }
-
-        _calculate() {
-            // Only re-calculate when one of the screen size breakpoints changes the size of the component
-            if (this._productcarousel_root.offsetWidth == this._currentRootWidth) {
-                return;
-            }
-
-            this._minPos = this._product_carousel_parent.offsetWidth - this._cards.length * this._cards[0].offsetWidth;
-            this._cardsContainer.style.width = this._cards[0].offsetWidth * this._cards.length + 'px';
-            this._maxPosIndex =
-                (this._cardsContainer.offsetWidth - this._product_carousel_parent.offsetWidth) /
-                this._cards[0].offsetWidth;
-
-            if (this._minPos >= 0) {
-                // Hide buttons if all fit on the screen
-                this._btnNext.style.display = 'none';
-                this._btnPrev.style.display = 'none';
-            } else {
-                this._btnNext.style.display = 'block';
-                this._btnPrev.style.display = 'block';
-                this._btnPrev.disabled = true;
-            }
-
-            // Reset carousel to first item
-            this._goToProductCard(0, 'reset');
-            this._currentRootWidth = this._productcarousel_root.offsetWidth;
-        }
-
-        // click event handler for Next Button
-        _goToNextProductCard() {
-            if (this._btnNext.disabled === false) {
-                var newCurrentPos = 0;
-                newCurrentPos = this._currentPos + 1;
-                this._goToProductCard(newCurrentPos, 'next');
-            }
-        }
-
-        // Click event handler for Prev Button
-        _goToPrevProductCard() {
-            if (this._btnPrev.disabled === false) {
-                var newCurrentPos = 0;
-                newCurrentPos = this._currentPos - 1;
-                this._goToProductCard(newCurrentPos, 'prev');
-            }
-        }
-
-        // create product card and transition
-        _goToProductCard(n, dir) {
-            var cardwidth = this._cards[0].offsetWidth,
-                currentPos = Math.max(-cardwidth * this._currentPos, this._minPos),
-                scrollWidth = cardwidth,
-                newPos;
-
-            if (dir === 'next') {
-                newPos = Math.max(this._minPos, currentPos - scrollWidth);
-            } else if (dir === 'prev') {
-                newPos = Math.min(0, currentPos + scrollWidth);
-            } else {
-                newPos = 0;
-            }
-
-            this._cardsContainer.style.transition =
-                'margin-left ' + this._speed + 'ms' + ' ' + this._effect + ' ' + this._delay + 'ms';
-            this._cardsContainer.style.marginLeft = newPos == 0 && this._minPos >= 0 ? 'auto' : newPos + 'px';
-            this._currentPos = n;
-
-            this._btnNext.disabled = false;
-            this._btnPrev.disabled = false;
-
-            if (this._currentPos >= this._maxPosIndex) {
-                this._btnNext.disabled = true;
-                this._btnPrev.disabled = false;
-            }
-
-            if (this._currentPos <= 0) {
-                this._btnPrev.disabled = true;
-                this._btnNext.disabled = false;
-            }
-        }
-    }
 
     function onDocumentReady() {
         const productCmp = document.querySelectorAll(selectors.self);
         if (productCmp) {
             productCmp.forEach(function(element) {
-                new ProductCarousel(element);
+                new Carousel(element, selectors);
             });
         }
     }
