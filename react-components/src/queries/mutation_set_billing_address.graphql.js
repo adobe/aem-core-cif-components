410852a172ecf20937fa2ebe16f131e9d68fdd08
Laurentiu Magureanu <magurean@adobe.com>
2021-03-18 18:17:11 +0200

    CIF-1996 - Add copyright headers to CSS and graphql files (#518)
    * CIF-1996: Add license header to css and graphql
    * CIF-1996: Fix jest tests
    * CIF-1996: Removed jest-transform-graphql
    * CIF-1996: Updated schema validation
    * CIF-1996: remove rat exceptions for css & graphql

---
+++ react-components/src/queries/mutation_set_billing_address.graphql.js
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ *
+ *    Copyright 2021 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+import { gql } from '@apollo/client';
+
+export default gql`
+    # Welcome to Altair GraphQL Client.
+    # You can send your request using CmdOrCtrl + Enter.
+
+    # Enter your graphQL query here.
+
+    mutation(
+        $cartId: String!
+        $city: String!
+        $company: String
+        $country_code: String!
+        $firstname: String!
+        $lastname: String!
+        $postcode: String
+        $region_code: String
+        $save_in_address_book: Boolean
+        $street: [String]!
+        $telephone: String!
+    ) {
+        setBillingAddressOnCart(
+            input: {
+                cart_id: $cartId
+                billing_address: {
+                    address: {
+                        city: $city
+                        company: $company
+                        country_code: $country_code
+                        firstname: $firstname
+                        lastname: $lastname
+                        postcode: $postcode
+                        region: $region_code
+                        save_in_address_book: $save_in_address_book
+                        street: $street
+                        telephone: $telephone
+                    }
+                }
+            }
+        ) {
+            cart {
+                billing_address {
+                    city
+                    company
+                    country {
+                        code
+                    }
+                    firstname
+                    lastname
+                    postcode
+                    region {
+                        code
+                    }
+                    street
+                    telephone
+                }
+            }
+        }
+    }
+`;
---
+++ react-components/src/queries/mutation_update_customer_address.graphql.js
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ *
+ *    Copyright 2021 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+import { gql } from '@apollo/client';
+
+export default gql`
+    mutation(
+        $id: Int!
+        $city: String
+        $company: String
+        $country_code: CountryCodeEnum
+        $default_billing: Boolean
+        $default_shipping: Boolean
+        $firstname: String
+        $lastname: String
+        $postcode: String
+        $region: CustomerAddressRegionInput
+        $street: [String]
+        $telephone: String
+    ) {
+        updateCustomerAddress(
+            id: $id
+            input: {
+                city: $city
+                company: $company
+                country_code: $country_code
+                default_billing: $default_billing
+                default_shipping: $default_shipping
+                firstname: $firstname
+                lastname: $lastname
+                postcode: $postcode
+                region: $region
+                street: $street
+                telephone: $telephone
+            }
+        ) {
+            id
+            city
+            company
+            country_code
+            default_billing
+            default_shipping
+            firstname
+            lastname
+            postcode
+            region {
+                region_code
+            }
+            street
+            telephone
+        }
+    }
+`;
