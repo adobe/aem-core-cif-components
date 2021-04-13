47342bf3543b3f0cd7c15ebc7554c7295e623208
Levente Sántha <levente@adobe.com>
2021-04-13 14:32:15 +0300

    CIF-1974 - Core Components: navigation - use UID for category queries (#528)
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * added category UID support for navigation component
    
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * fixed test failure
    
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * added support for URLProvider driven UID rendering in URLs
    
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * fixed test failure
    
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * fixed build failure
    
    * CIF-1974 - Core Components: navigation - use UID for category queries
    
     * review feedback

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/navigation/GraphQLCategoryProvider.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/navigation/GraphQLCategoryProvider.java
@@ -18,6 +18,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.slf4j.Logger;
@@ -25,9 +26,11 @@ import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.commerce.core.components.client.MagentoGraphqlClient;
 import com.adobe.cq.commerce.graphql.client.GraphqlResponse;
+import com.adobe.cq.commerce.magento.graphql.CategoryFilterInput;
 import com.adobe.cq.commerce.magento.graphql.CategoryTree;
 import com.adobe.cq.commerce.magento.graphql.CategoryTreeQuery;
 import com.adobe.cq.commerce.magento.graphql.CategoryTreeQueryDefinition;
+import com.adobe.cq.commerce.magento.graphql.FilterEqualTypeInput;
 import com.adobe.cq.commerce.magento.graphql.Operations;
 import com.adobe.cq.commerce.magento.graphql.Query;
 import com.adobe.cq.commerce.magento.graphql.QueryQuery;
@@ -37,31 +40,39 @@ import com.day.cq.wcm.api.Page;
 class GraphQLCategoryProvider {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GraphQLCategoryProvider.class);
-    private static final Function<CategoryTreeQuery, CategoryTreeQuery> CATEGORIES_QUERY = q -> q.id().name().urlPath().position();
+    private static final Function<CategoryTreeQuery, CategoryTreeQuery> CATEGORIES_QUERY = q -> q.id().uid().name().urlPath().position();
     private MagentoGraphqlClient magentoGraphqlClient;
 
     GraphQLCategoryProvider(Resource resource, Page page, SlingHttpServletRequest request) {
         magentoGraphqlClient = MagentoGraphqlClient.create(resource, page, request);
     }
 
-    List<CategoryTree> getChildCategories(Integer categoryId, Integer depth) {
-        if (magentoGraphqlClient == null || categoryId == null) {
+    List<CategoryTree> getChildCategories(String categoryIdentifier, Integer depth, boolean supportUID) {
+        if (magentoGraphqlClient == null) {
             LOGGER.debug("No Graphql client present, cannot retrieve top categories");
             return Collections.emptyList();
         }
 
-        QueryQuery.CategoryArgumentsDefinition searchArgs = q -> q.id(categoryId);
-        String queryString = Operations.query(query -> query.category(searchArgs, defineCategoriesQuery(depth))).toString();
-        GraphqlResponse<Query, Error> response = magentoGraphqlClient.execute(queryString);
-
-        Query rootQuery = response.getData();
-        CategoryTree category = rootQuery.getCategory();
-        if (category == null) {
-            LOGGER.warn("Magento category not found for id: " + categoryId);
+        if (StringUtils.isBlank(categoryIdentifier)) {
+            LOGGER.debug("Empty category identifier");
             return Collections.emptyList();
         }
 
-        List<CategoryTree> children = category.getChildren();
+        CategoryFilterInput categoryFilter = supportUID ? new CategoryFilterInput().setCategoryUid(new FilterEqualTypeInput().setEq(
+            categoryIdentifier)) : new CategoryFilterInput().setIds(new FilterEqualTypeInput().setEq(categoryIdentifier));
+        QueryQuery.CategoryListArgumentsDefinition searchArgs = d -> d.filters(categoryFilter);
+
+        String queryString = Operations.query(query -> query.categoryList(searchArgs, defineCategoriesQuery(depth))).toString();
+        GraphqlResponse<Query, Error> response = magentoGraphqlClient.execute(queryString);
+
+        Query rootQuery = response.getData();
+        List<CategoryTree> category = rootQuery.getCategoryList();
+        if (category.isEmpty()) {
+            LOGGER.warn("Magento category not found for identifier: " + categoryIdentifier);
+            return Collections.emptyList();
+        }
+
+        List<CategoryTree> children = category.get(0).getChildren();
         if (children == null) {
             return Collections.emptyList();
         }
