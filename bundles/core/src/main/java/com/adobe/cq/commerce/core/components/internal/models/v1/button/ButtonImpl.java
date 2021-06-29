c71aaa0e3d8737a5435d14a39c6a1beffa9e00ba
drudolph <drudolph@adobe.com>
2021-06-29 17:20:30 +0200

    force MagentoGraphqlClient to be optional for ButtonImpl

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/button/ButtonImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/models/v1/button/ButtonImpl.java
@@ -24,6 +24,7 @@ import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
@@ -84,7 +85,7 @@ public class ButtonImpl implements Button {
     @Via(type = ResourceSuperType.class)
     private Button button;
 
-    @Self
+    @Self(injectionStrategy = InjectionStrategy.OPTIONAL)
     private MagentoGraphqlClient magentoGraphqlClient;
 
     @ValueMapValue
