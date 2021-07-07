ff476dc2384ee88fbbd37afd4fc9facf2a632b0a
Markus Haack <mhaack@adobe.com>
2021-07-07 16:30:03 +0200

    CIF-2062 - fix OSGI config

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/services/UrlProviderConfiguration.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/services/UrlProviderConfiguration.java
@@ -16,20 +16,51 @@ package com.adobe.cq.commerce.core.components.internal.services;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.Option;
 
+import com.adobe.cq.commerce.core.components.internal.services.urlformats.CategoryPageWithUrlKey;
 import com.adobe.cq.commerce.core.components.internal.services.urlformats.CategoryPageWithUrlPath;
+import com.adobe.cq.commerce.core.components.internal.services.urlformats.ProductPageWithSku;
+import com.adobe.cq.commerce.core.components.internal.services.urlformats.ProductPageWithSkuAndUrlKey;
+import com.adobe.cq.commerce.core.components.internal.services.urlformats.ProductPageWithSkuAndUrlPath;
 import com.adobe.cq.commerce.core.components.internal.services.urlformats.ProductPageWithUrlKey;
+import com.adobe.cq.commerce.core.components.internal.services.urlformats.ProductPageWithUrlPath;
 
 @ObjectClassDefinition(name = "CIF URL Provider configuration")
 public @interface UrlProviderConfiguration {
 
-    // TODO these should be dropdown, add "options" to annotation
-
     @AttributeDefinition(
-        name = "Product page url format")
+        name = "Product page url format",
+        description = "Defines the format of a product page URL.",
+        options = {
+            @Option(
+                label = "Suffix with product sku : " + ProductPageWithSku.PATTERN,
+                value = ProductPageWithSku.PATTERN),
+            @Option(
+                label = "Suffix with product sku & url_key : " + ProductPageWithSkuAndUrlKey.PATTERN,
+                value = ProductPageWithSkuAndUrlKey.PATTERN),
+            @Option(
+                label = "Suffix with product sku & url_path : " + ProductPageWithSkuAndUrlPath.PATTERN,
+                value = ProductPageWithSkuAndUrlPath.PATTERN),
+            @Option(
+                label = "Suffix with product url_key : " + ProductPageWithUrlKey.PATTERN,
+                value = ProductPageWithUrlKey.PATTERN),
+            @Option(
+                label = "Suffix with product url_path : " + ProductPageWithUrlPath.PATTERN,
+                value = ProductPageWithUrlPath.PATTERN)
+        })
     String productPageUrlFormat() default ProductPageWithUrlKey.PATTERN;
 
     @AttributeDefinition(
-        name = "Category page url format")
+        name = "Category page url format",
+        description = "Defines the format of a category page URL.",
+        options = {
+            @Option(
+                label = "Suffix with product url_key : " + CategoryPageWithUrlKey.PATTERN,
+                value = CategoryPageWithUrlKey.PATTERN),
+            @Option(
+                label = "Suffix with product url_path : " + CategoryPageWithUrlPath.PATTERN,
+                value = CategoryPageWithUrlPath.PATTERN)
+        })
     String categoryPageUrlFormat() default CategoryPageWithUrlPath.PATTERN;
 }
