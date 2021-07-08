197b71c865f3f2befff42e970f528a2f33b77203
Laurentiu Magureanu <magurean@adobe.com>
2021-07-08 13:40:50 +0300

    CIF-2173: Fix merge conflict

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/services/urlformats/AbstractUrlFormat.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/services/urlformats/AbstractUrlFormat.java
@@ -13,13 +13,43 @@
  ******************************************************************************/
 package com.adobe.cq.commerce.core.components.internal.services.urlformats;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.sling.api.resource.ResourceUtil;
 
 import com.adobe.cq.commerce.core.components.internal.services.UrlFormat;
 
+import static com.adobe.cq.commerce.core.components.services.UrlProvider.URL_KEY_PARAM;
+import static com.adobe.cq.commerce.core.components.services.UrlProvider.URL_PATH_PARAM;
+
 abstract class AbstractUrlFormat implements UrlFormat {
     protected static final String HTML_EXTENSION = ".html";
+
+    /**
+     * Returns the url_key from the given paramters. If not present but the url_path is, the last segment of the url_path os returned
+     * instead.
+     * <p>
+     * Returns {@code "{{" + URL_KEY_PARAM + "}}"} when the url_key is empty
+     *
+     * @param parameters
+     * @return
+     */
+    protected static String getUrlKey(Map<String, String> parameters) {
+        String urlKey = null;
+        if (StringUtils.isEmpty(parameters.get(URL_KEY_PARAM))) {
+            if (StringUtils.isNotEmpty(parameters.get(URL_PATH_PARAM))) {
+                urlKey = StringUtils.substringAfterLast(parameters.get(URL_PATH_PARAM), "/");
+                if (StringUtils.isEmpty(urlKey)) {
+                    urlKey = parameters.get(URL_PATH_PARAM);
+                }
+            }
+        } else {
+            urlKey = parameters.get(URL_KEY_PARAM);
+        }
+        return StringUtils.isNotEmpty(urlKey) ? urlKey : "{{" + URL_KEY_PARAM + "}}";
+    }
 
     protected static String removeJcrContent(String path) {
         if (path == null) {
