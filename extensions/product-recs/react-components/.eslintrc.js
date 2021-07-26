d09e7774b2a3461c12ceb635bda12f4c0d1b2ca7
Dirk Rudolph <drudolph@adobe.com>
2021-07-26 13:18:09 +0200

    fix eslintrc

--- extensions/product-recs/react-components/.eslintrc.js
+++ extensions/product-recs/react-components/.eslintrc.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line header/header
 const headerBlock = [
-    '/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'',
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
     {
         pattern: ' ~ Copyright \\d{4} Adobe',
         template: ` ~ Copyright ${new Date().getFullYear()} Adobe`,
-    }
+    },
     ' ~',
     ' ~ Licensed under the Apache License, Version 2.0 (the "License");',
     ' ~ you may not use this file except in compliance with the License.',
@@ -17,7 +17,7 @@ const headerBlock = [
     ' ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
     ' ~ See the License for the specific language governing permissions and',
     ' ~ limitations under the License.',
-    ' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/',
+    ' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
 ];
 
 module.exports = {
