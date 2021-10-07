18af7a14e8e1bbe55651f94cdf2675b216433212
Levente Santha <levente@adobe.com>
2021-10-07 17:57:16 +0300

    CIF-2388 - Rewrite category/product links
     * added Sling rewriter for link transformation
     * added unit tests

---
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/components/internal/services/CifLinksTransformerFactory.java
@@ -0,0 +1,114 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2021 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.commerce.core.components.internal.services;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.rewriter.DefaultTransformer;
+import org.apache.sling.rewriter.ProcessingComponentConfiguration;
+import org.apache.sling.rewriter.ProcessingContext;
+import org.apache.sling.rewriter.Transformer;
+import org.apache.sling.rewriter.TransformerFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import com.adobe.cq.commerce.core.components.services.urls.UrlProvider;
+import com.adobe.cq.commerce.core.components.utils.SiteNavigation;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+/**
+ * Sling rewriter for transforming links tagged with (@code data-category-uid} or {@code data-product-sku} to
+ * PDP or PLP links according to the UrlProvider configuration.
+ */
+@Component(
+    immediate = true,
+    service = TransformerFactory.class,
+    property = {
+        "pipeline.type=ciflinks"
+    })
+public class CifLinksTransformerFactory implements TransformerFactory {
+    static final String ATTR_CATEGORY_UID = "data-category-uid";
+    static final String ATTR_PRODUCT_SKU = "data-product-sku";
+    public static final String ATTR_HREF = "href";
+    public static final String ELEMENT_ANCHOR = "a";
+
+    @Reference
+    UrlProvider urlProvider;
+
+    @Override
+    public Transformer createTransformer() {
+        return new CifLinksTransformer();
+    }
+
+    class CifLinksTransformer extends DefaultTransformer {
+        private SlingHttpServletRequest request;
+
+        @Override
+        public void init(ProcessingContext context, ProcessingComponentConfiguration config) throws IOException {
+            this.request = context.getRequest();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (ELEMENT_ANCHOR.equals(localName)) {
+                String categoryUid = attributes.getValue(ATTR_CATEGORY_UID);
+                String newHref = null;
+                if (StringUtils.isNotBlank(categoryUid)) {
+                    Page currentPage = request.getResourceResolver().adaptTo(PageManager.class).getContainingPage(request.getResource());
+                    Page categoryPage = SiteNavigation.getCategoryPage(currentPage);
+                    newHref = urlProvider.toCategoryUrl(request, categoryPage, categoryUid);
+                }
+                String productSku = attributes.getValue(ATTR_PRODUCT_SKU);
+                if (StringUtils.isNotBlank(productSku)) {
+                    Page currentPage = request.getResourceResolver().adaptTo(PageManager.class).getContainingPage(request.getResource());
+                    Page productPage = SiteNavigation.getProductPage(currentPage);
+                    newHref = urlProvider.toProductUrl(request, productPage, productSku);
+                }
+
+                if (newHref != null) {
+                    String href = attributes.getValue(ATTR_HREF);
+                    AttributesImpl attributesImpl;
+                    if (StringUtils.isNotBlank(href)) {
+                        attributesImpl = new AttributesImpl(attributes);
+                        attributesImpl.setValue(attributes.getIndex(ATTR_HREF), newHref);
+                    } else {
+                        attributesImpl = new AttributesImpl(attributes);
+                        attributesImpl.addAttribute("", ATTR_HREF, ATTR_HREF, "CDATA", newHref);
+                    }
+                    int index = attributesImpl.getIndex(ATTR_CATEGORY_UID);
+                    if (index > -1) {
+                        attributesImpl.removeAttribute(index);
+                    }
+                    index = attributesImpl.getIndex(ATTR_PRODUCT_SKU);
+                    if (index > -1) {
+                        attributesImpl.removeAttribute(index);
+                    }
+                    super.startElement(uri, localName, qName, attributesImpl);
+                } else {
+                    super.startElement(uri, localName, qName, attributes);
+                }
+            } else {
+                super.startElement(uri, localName, qName, attributes);
+            }
+        }
+    }
+}
---
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/components/internal/services/CifLinksTransformerTest.java
@@ -0,0 +1,128 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2021 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+package com.adobe.cq.commerce.core.components.internal.services;
+
+import java.io.StringWriter;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.osgi.services.HttpClientBuilderFactory;
+import org.apache.jackrabbit.commons.xml.ParsingContentHandler;
+import org.apache.jackrabbit.commons.xml.ToXmlContentHandler;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.rewriter.ProcessingContext;
+import org.apache.sling.rewriter.Transformer;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.adobe.cq.commerce.core.MockHttpClientBuilderFactory;
+import com.adobe.cq.commerce.core.testing.Utils;
+import com.adobe.cq.commerce.graphql.client.GraphqlClient;
+import com.adobe.cq.commerce.graphql.client.impl.GraphqlClientImpl;
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+import static com.adobe.cq.commerce.core.components.internal.services.CifLinksTransformerFactory.ATTR_CATEGORY_UID;
+import static com.adobe.cq.commerce.core.components.internal.services.CifLinksTransformerFactory.ATTR_HREF;
+import static com.adobe.cq.commerce.core.components.internal.services.CifLinksTransformerFactory.ATTR_PRODUCT_SKU;
+import static com.adobe.cq.commerce.core.components.internal.services.CifLinksTransformerFactory.ELEMENT_ANCHOR;
+import static com.adobe.cq.commerce.core.testing.TestContext.newAemContext;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class CifLinksTransformerTest {
+    private static final String TEST_PAGE = "/content/pageA";
+    private static final String TEST_HTML = "rewriter/ciflinks.html";
+    @Rule
+    public final AemContext context = newAemContext("/context/jcr-content.json");
+    private Transformer transformer;
+
+    @Before
+    public void before() throws Exception {
+        // setup GraphQL client for UrlProvider
+        CloseableHttpClient httpClient = mock(CloseableHttpClient.class);
+        context.registerService(HttpClientBuilderFactory.class, new MockHttpClientBuilderFactory(httpClient));
+        GraphqlClientImpl graphqlClient = spy(new GraphqlClientImpl());
+        context.registerInjectActivateService(graphqlClient, "httpMethod", "POST");
+        context.registerAdapter(Resource.class, GraphqlClient.class, graphqlClient);
+        Utils.setupHttpResponse("graphql/magento-graphql-product-result.json", httpClient, HttpStatus.SC_OK,
+            "{products(filter:{sku:{eq:\"MJ01\"}}");
+        Utils.setupHttpResponse("graphql/magento-graphql-category-list-result.json", httpClient, HttpStatus.SC_OK,
+            "{categoryList(filters:{category_uid:{eq:\"uid-5\"}}");
+
+        // setup UrlRewriterTransformer
+        MockSlingHttpServletRequest mockRequest = context.request();
+        mockRequest.setResource(context.resourceResolver().getResource(TEST_PAGE));
+        ProcessingContext mockProcessingContext = mock(ProcessingContext.class);
+        when(mockProcessingContext.getRequest()).thenReturn(mockRequest);
+        CifLinksTransformerFactory factory = new CifLinksTransformerFactory();
+        context.registerInjectActivateService(factory);
+        transformer = factory.createTransformer();
+        transformer.init(mockProcessingContext, null);
+    }
+
+    @Test
+    public void testTransformer() throws Exception {
+        // read and transform HTML
+        StringWriter writer = new StringWriter();
+        transformer.setContentHandler(new ToXmlContentHandler(writer));
+        ParsingContentHandler parsingContentHandler = new ParsingContentHandler(transformer);
+        parsingContentHandler.parse(Utils.class.getClassLoader().getResourceAsStream(TEST_HTML));
+
+        // verify transformed HTML
+        String html = writer.toString();
+        Document document = Jsoup.parse(html);
+        Elements anchors = document.select(ELEMENT_ANCHOR);
+
+        assertEquals(6, anchors.size());
+        checkAnchor(anchors.get(0), "/content/category-page.html/equipment.html");
+        checkAnchor(anchors.get(1), "/content/category-page.html/equipment.html");
+        checkAnchor(anchors.get(2), null);
+        checkAnchor(anchors.get(3), "href");
+        checkAnchor(anchors.get(4), "/content/product-page.html/beaumont-summit-kit.html");
+        checkAnchor(anchors.get(5), "/content/product-page.html/beaumont-summit-kit.html");
+    }
+
+    private void checkAnchor(Element anchor, String href) {
+        // id (arbitrary attributes) preserved
+        assertTrue(isNotBlank(anchor.attr("id")));
+        // text preserved
+        assertEquals(1, anchor.childNodeSize());
+        assertTrue(anchor.childNode(0) instanceof TextNode);
+        assertTrue(isNotBlank(((TextNode) anchor.childNode(0)).getWholeText()));
+        // CifLinks attributes removed
+        assertFalse(anchor.hasAttr(ATTR_PRODUCT_SKU));
+        assertFalse(anchor.hasAttr(ATTR_CATEGORY_UID));
+        // href matched
+        if (href == null) {
+            assertTrue(isBlank(anchor.attr(ATTR_HREF)));
+        } else {
+            assertEquals(href, anchor.attr(ATTR_HREF));
+        }
+    }
+}
