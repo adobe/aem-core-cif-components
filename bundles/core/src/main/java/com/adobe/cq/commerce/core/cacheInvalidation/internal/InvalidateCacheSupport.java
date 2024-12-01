0c71fb215dc4073e504c265f0493ca24a49e2cd4
Alwin Joseph <aljoseph@adobe.com>
2024-12-01 18:17:13 +0530

    SITES-21278: Adds Invalidation for in-memory & dispatcher cache

---
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/cacheInvalidation/internal/InvalidateCacheSupport.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ *
+ *    Copyright 2019 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.adobe.cq.commerce.core.cacheInvalidation.internal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+import com.adobe.cq.commerce.graphql.client.GraphqlClient;
+
+@Component(service = InvalidateCacheSupport.class, immediate = true)
+public class InvalidateCacheSupport {
+
+    public static final String INVALIDATE_WORKING_AREA = "/var/cif";
+    public static final String NODE_NAME_BASE = "invalidate_entry";
+    public static final String SERVICE_USER = "cif-flush";
+    public static final String PROPERTIES_GRAPHQL_CLIENT_ID = "cq:catalogIdentifier";
+    public static final String PROPERTIES_STORE_VIEW = "magentoStore";
+    public static final String PROPERTIES_TYPE = "type";
+    public static final String PROPERTIES_STORE_PATH = "storePath";
+    public static final String PROPERTIES_INVALID_CACHE_ENTRIES = "invalidCacheEntries";
+    public static final String PROPERTIES_ATTRIBUTE = "attribute";
+    public static final String PROPERTIES_LIST_OF_CACHE_TO_SEARCH = "listOfCacheToSearch";
+    public static final String TYPE_SKU = "skus";
+    public static final String TYPE_CATEGORY = "categories";
+    public static final String TYPE_UUIDS = "uuids";
+    public static final String TYPE_ClEAR_SPECIFIC_CACHE = "clearSpecificCache";
+    public static final String TYPE_ATTRIBUTE = "attribute";
+    public static final String TYPE_CLEAR_ALL = "clearAll";
+
+    private final Collection<ClientHolder> clients = new ArrayList<>();
+
+    public GraphqlClient getClient(String graphqlClientId) {
+        if (graphqlClientId != null && !graphqlClientId.isEmpty()) {
+            for (ClientHolder clientHolder : clients) {
+                GraphqlClient graphqlClient = clientHolder.graphqlClient;
+                Map<String, Object> properties = clientHolder.properties;
+                String identifier = (String) properties.get("identifier");
+                if (identifier.equals(graphqlClientId)) {
+                    return graphqlClient;
+                }
+            }
+        }
+        throw new IllegalStateException("GraphqlClient with ID '" + graphqlClientId + "' not found");
+    }
+
+    @Reference(
+        service = GraphqlClient.class,
+        bind = "bindGraphqlClient",
+        unbind = "unbindGraphqlClient",
+        cardinality = ReferenceCardinality.MULTIPLE,
+        policy = ReferencePolicy.DYNAMIC)
+    void bindGraphqlClient(GraphqlClient graphqlClient, Map<String, Object> properties) {
+        clients.add(new ClientHolder(graphqlClient, properties));
+    }
+
+    void unbindGraphqlClient(GraphqlClient graphqlClient, Map<?, ?> properties) {
+        clients.removeIf(holder -> holder.graphqlClient.equals(graphqlClient));
+    }
+
+    private static class ClientHolder {
+        private final GraphqlClient graphqlClient;
+        private final Map<String, Object> properties;
+
+        ClientHolder(GraphqlClient graphqlClient, Map<String, Object> properties) {
+            this.graphqlClient = graphqlClient;
+            this.properties = properties;
+        }
+    }
+}
---
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/cacheInvalidation/internal/ServiceUserService.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ *
+ *    Copyright 2024 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.adobe.cq.commerce.core.cacheInvalidation.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = { ServiceUserService.class }, immediate = true)
+public class ServiceUserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceUserService.class);
+
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    public ResourceResolver getServiceUserResourceResolver(String serviceUser) {
+
+        ResourceResolver resourceResolver = null;
+        Map<String, Object> param = new HashMap<>();
+        param.put(ResourceResolverFactory.SUBSERVICE, serviceUser);
+
+        try {
+            // Get the service user ResourceResolver
+            resourceResolver = resourceResolverFactory.getServiceResourceResolver(param);
+            LOG.info("Successfully obtained ResourceResolver for service user: {}", serviceUser);
+        } catch (LoginException e) {
+            LOG.error("Failed to obtain ResourceResolver for service user: {}", serviceUser, e);
+        }
+
+        return resourceResolver;
+    }
+}
