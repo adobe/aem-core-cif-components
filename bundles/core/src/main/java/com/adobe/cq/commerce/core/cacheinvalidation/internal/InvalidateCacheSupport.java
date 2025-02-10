e4aeec50ddb1454af23bb526cd51d2083acdbc97
levente <levente@adobe.com>
2025-02-10 16:12:02 +0200

    * fixed invalidation entry name * updated cif ask api version

--- bundles/core/src/main/java/com/adobe/cq/commerce/core/cacheinvalidation/internal/InvalidateCacheSupport.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/core/cacheinvalidation/internal/InvalidateCacheSupport.java
@@ -32,7 +32,7 @@ import com.adobe.cq.commerce.graphql.client.GraphqlClient;
 public class InvalidateCacheSupport {
 
     public static final String INVALIDATE_WORKING_AREA = "/var/cif/cacheInvalidation";
-    public static final String NODE_NAME_BASE = "invalidate_entry";
+    public static final String NODE_NAME_BASE = "cmd";
     public static final String SERVICE_USER = "cif-cache-invalidation-service";
     public static final String PROPERTIES_GRAPHQL_CLIENT_ID = "cq:graphqlClient";
     public static final String PROPERTIES_STORE_VIEW = "magentoStore";
