0f48f1a7bcbd491f0df1cc8c83205ff6d734c75d
Alwin Joseph <aljoseph@adobe.com>
2025-02-19 10:54:05 +0530

    SITES-29323: Renaming the file name

--- bundles/core/src/test/java/com/adobe/cq/commerce/core/testing/GraphqlUniqueMessageLogBackFilter.java
+++ bundles/core/src/test/java/com/adobe/cq/commerce/core/testing/GraphqlUniqueMessageLogBackFilter.java
@@ -22,7 +22,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class UniqueMessageLogBackFilter extends Filter<ILoggingEvent> {
+public class GraphqlUniqueMessageLogBackFilter extends Filter<ILoggingEvent> {
     private final Set<String> messageSet = new HashSet<>();
 
     @Override
