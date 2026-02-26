7f6d19bf6f5b2b7a8a7676e94cc1fe28171e9dc7
Alwin Joseph <aljoseph@adobe.com>
2026-02-26 07:45:55 +0530

    SITES-41041: Fix the integration issue

--- it/http/src/test/java/com/adobe/cq/commerce/it/http/VersionHistoryPreviewIT.java
+++ it/http/src/test/java/com/adobe/cq/commerce/it/http/VersionHistoryPreviewIT.java
@@ -15,12 +15,15 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.commerce.it.http;
 
+import org.apache.http.HttpEntity;
 import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.SlingHttpResponse;
+import org.apache.sling.testing.clients.util.FormEntityBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -29,27 +32,39 @@ import static org.junit.Assert.assertTrue;
 public class VersionHistoryPreviewIT extends CommerceTestBase {
 
     private static final String SOURCE_PRODUCT_TEASER_PAGE = COMMERCE_LIBRARY_PATH + "/productteaser";
-    private static final String VERSION_HISTORY_ROOT = "/tmp/versionhistory/cif-it-hash/cif-it-version";
-    private static final String VERSION_HISTORY_PARENT = VERSION_HISTORY_ROOT + "/content/core-components-examples/library/commerce";
-    private static final String VERSION_HISTORY_PAGE_NODE = VERSION_HISTORY_PARENT + "/productteaser";
-    private static final String VERSION_HISTORY_PAGE_PATH = VERSION_HISTORY_PARENT + "/productteaser.html";
+    private static final String VERSION_HISTORY_ROOT_BASE = "/tmp/versionhistory/cif-it-hash";
+    private static final String VERSION_HISTORY_PAGE_SUFFIX = "/content/core-components-examples/library/commerce/productteaser";
     private static final String PRODUCT_TEASER_SELECTOR = CMP_EXAMPLES_DEMO_SELECTOR + " .productteaser .item__name > span";
+    private String versionHistoryRoot;
+    private String versionHistoryPagePath;
+
+    @Before
+    public void setup() throws ClientException {
+        assertTrue("Source page missing: " + SOURCE_PRODUCT_TEASER_PAGE, adminAuthor.exists(SOURCE_PRODUCT_TEASER_PAGE));
+        versionHistoryRoot = VERSION_HISTORY_ROOT_BASE + "/cif-it-version-" + System.currentTimeMillis();
+        String versionHistoryParent = versionHistoryRoot + "/content/core-components-examples/library/commerce";
+        String versionHistoryPageNode = versionHistoryRoot + VERSION_HISTORY_PAGE_SUFFIX;
+        versionHistoryPagePath = versionHistoryPageNode + ".html";
+
+        adminAuthor.createNodeRecursive(versionHistoryParent, "sling:Folder");
+        HttpEntity copyEntity = FormEntityBuilder.create()
+            .addParameter(":operation", "copy")
+            .addParameter(":dest", versionHistoryPageNode)
+            .build();
+        adminAuthor.doPost(SOURCE_PRODUCT_TEASER_PAGE, copyEntity, 200, 201);
+        assertTrue("Version history preview page was not created", adminAuthor.exists(versionHistoryPageNode));
+    }
 
     @After
     public void cleanup() throws ClientException {
-        if (adminAuthor.exists(VERSION_HISTORY_ROOT)) {
-            adminAuthor.deletePath(VERSION_HISTORY_ROOT);
+        if (versionHistoryRoot != null && adminAuthor.exists(versionHistoryRoot)) {
+            adminAuthor.deletePath(versionHistoryRoot);
         }
     }
 
     @Test
     public void testVersionHistoryPathRendersProductTeaser() throws ClientException {
-        assertTrue("Source page missing: " + SOURCE_PRODUCT_TEASER_PAGE, adminAuthor.exists(SOURCE_PRODUCT_TEASER_PAGE));
-        adminAuthor.createNodeRecursive(VERSION_HISTORY_PARENT, "sling:Folder");
-        adminAuthor.copyPage(new String[] { SOURCE_PRODUCT_TEASER_PAGE }, "productteaser", null, VERSION_HISTORY_PARENT, null, false);
-        assertTrue("Version history preview page was not created", adminAuthor.exists(VERSION_HISTORY_PAGE_NODE));
-
-        SlingHttpResponse response = adminAuthor.doGet(VERSION_HISTORY_PAGE_PATH, 200);
+        SlingHttpResponse response = adminAuthor.doGet(versionHistoryPagePath, 200);
         Document doc = Jsoup.parse(response.getContent());
         Elements elements = doc.select(PRODUCT_TEASER_SELECTOR);
         assertEquals("Summit Watch", elements.first().html());
