e87c424af84e8b202a9d9a6db58cd929cc89937a
spothabathula <del60718@adobe.com>
2026-02-27 19:31:00 +0530

    Revert "initial commit"
    This reverts commit 12a4c3253cf5f77b12c16a2f9388ad476d9aba17.

--- react-components/src/components/Checkout/paymentProviders/braintree.js
+++ react-components/src/components/Checkout/paymentProviders/braintree.js
@@ -17,6 +17,7 @@ import React, { useEffect, useState } from 'react';
 
 import { useCheckoutState } from '../checkoutContext';
 import { useMutation } from '@apollo/client';
+import dropIn from 'braintree-web-drop-in';
 import CREATE_BRAINTREE_CLIENT_TOKEN from '../../../queries/mutation_create_braintree_client_token.graphql';
 import { oneOf } from 'prop-types';
 import { useFieldApi } from 'informed';
@@ -38,7 +39,7 @@ const Braintree = props => {
 
     async function createDropinInstance() {
         try {
-            const { default: dropIn } = await import('braintree-web-drop-in');
+            // Tear down instance if it already exists, e.g. when switching between the PayPal and credit card form.
             if (dropinInstance) {
                 await dropinInstance.teardown();
                 setDropinInstance(false);
