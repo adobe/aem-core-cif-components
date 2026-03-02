8bdf199e2cc8b42df8737f68dca48ad36e25a011
spothabathula <del60718@adobe.com>
2026-03-02 16:54:51 +0530

    initial commit

--- react-components/webpack.config.js
+++ react-components/webpack.config.js
@@ -22,11 +22,45 @@ const libraryName = pkg.name;
 const externals = Object.keys(pkg.peerDependencies)
     .reduce((obj, key) => ({ ...obj, [key]: `commonjs ${key}`}), {});
 
+const entriesDir = path.resolve(__dirname, 'src', 'entries');
+const componentEntries = {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+    CommerceApp: path.join(entriesDir, 'CommerceApp.js'),
+    AuthBar: path.join(entriesDir, 'AuthBar.js'),
+    Cart: path.join(entriesDir, 'Cart.js'),
+    CartTrigger: path.join(entriesDir, 'CartTrigger.js'),
+    AccountContainer: path.join(entriesDir, 'AccountContainer.js'),
+    AddressBook: path.join(entriesDir, 'AddressBook.js'),
+    BundleProductOptions: path.join(entriesDir, 'BundleProductOptions.js'),
+    GiftCardOptions: path.join(entriesDir, 'GiftCardOptions.js'),
+    Portal: path.join(entriesDir, 'Portal.js'),
+    PortalPlacer: path.join(entriesDir, 'PortalPlacer.js'),
+    ResetPassword: path.join(entriesDir, 'ResetPassword.js'),
+    Price: path.join(entriesDir, 'Price.js'),
+    Trigger: path.join(entriesDir, 'Trigger.js'),
+    LoadingIndicator: path.join(entriesDir, 'LoadingIndicator.js'),
+    ConfigContext: path.join(entriesDir, 'ConfigContext.js'),
+    UserContext: path.join(entriesDir, 'UserContext.js'),
+    CheckoutProvider: path.join(entriesDir, 'CheckoutProvider.js'),
+    AccountDetails: path.join(entriesDir, 'AccountDetails.js'),
+    MyAccount: path.join(entriesDir, 'MyAccount.js'),
+    authUtils: path.join(entriesDir, 'authUtils.js'),
+    hooks: path.join(entriesDir, 'hooks.js'),
+    createProductPageUrl: path.join(entriesDir, 'createProductPageUrl.js'),
+    useDataLayerEvents: path.join(entriesDir, 'useDataLayerEvents.js'),
+    useAddToCart: path.join(entriesDir, 'useAddToCart.js'),
+    useAddToWishlistEvent: path.join(entriesDir, 'useAddToWishlistEvent.js'),
+    useCustomUrlEvent: path.join(entriesDir, 'useCustomUrlEvent.js'),
+    useReferrerEvent: path.join(entriesDir, 'useReferrerEvent.js'),
+    usePageEvent: path.join(entriesDir, 'usePageEvent.js'),
+    dataLayerUtils: path.join(entriesDir, 'dataLayerUtils.js')
+};
+
 module.exports = {
-    entry: path.resolve(__dirname, 'src') + '/index.js',
+    entry: componentEntries,
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: '[name].js',
         libraryTarget: 'umd',
         library: libraryName,
         umdNamedDefine: true,
