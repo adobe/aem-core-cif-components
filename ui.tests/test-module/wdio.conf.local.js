2728dae4184ba0c5bf50dc7bd8b9b71a0e02986b
spothabathula <del60718@adobe.com>
2026-04-07 20:58:49 +0530

    cleanup

--- ui.tests/test-module/wdio.conf.local.js
+++ ui.tests/test-module/wdio.conf.local.js
@@ -15,6 +15,7 @@
  */
 
 /**
+ * DO NOT MODIFY
  * Local WDIO overrides (browser, selenium-standalone). Shared defaults: wdio.conf.commons.js
  */
 let wdio_config = require('./wdio.conf.commons.js').config;
